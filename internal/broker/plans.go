49248a817a87d78e6efdb2a2c40ab96ed007d7b6
Marek Michali <marek.michali@sap.com>
2023-09-12 07:16:01 +0200

    Add missing region

--- internal/broker/plans.go
+++ internal/broker/plans.go
@@ -110,7 +110,7 @@ func AWSRegions(euRestrictedAccess bool) []string {
 }
 
 func OpenStackRegions() []string {
-	return []string{"eu-de-1", "ap-sa-1"}
+	return []string{"eu-de-2", "eu-de-1", "ap-sa-1"}
 }
 
 func requiredSchemaProperties() []string {
