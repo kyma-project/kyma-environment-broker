10520d3e34f4dabab72cdb09d19a65ff7c929dd2
Marcin Szwed <marcin.szwed@sap.com>
2023-11-06 16:59:06 +0100

    Add fake operations queue and unit test

--- internal/expiration/handler_test.go
+++ internal/expiration/handler_test.go
@@ -1,1 +1,41 @@
-package expiration
+package expiration_test
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/kyma-project/kyma-environment-broker/internal/expiration"
+	"github.com/kyma-project/kyma-environment-broker/internal/process"
+	"github.com/kyma-project/kyma-environment-broker/internal/storage"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+const requestPathFormat = "/expire/service_instance/%s"
+
+func TestExpiration(t *testing.T) {
+	router := mux.NewRouter()
+	deprovisioningQueue := process.NewFakeQueue()
+	storage := storage.NewMemoryStorage()
+	logger := logrus.New()
+	handler := expiration.NewHandler(storage.Instances(), storage.Operations(), deprovisioningQueue, logger)
+	handler.AttachRoutes(router)
+
+	t.Run("should receive 404 Not Found response", func(t *testing.T) {
+		// given
+		instanceID := "inst-404-not-found"
+		reqPath := fmt.Sprintf(requestPathFormat, instanceID)
+		req := httptest.NewRequest("PUT", reqPath, nil)
+		w := httptest.NewRecorder()
+
+		// when
+		router.ServeHTTP(w, req)
+		resp := w.Result()
+
+		// then
+		assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+	})
+}
