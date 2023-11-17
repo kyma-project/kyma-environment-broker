afc4aff2fb33f679c8e68e10ab880ff95dcc1725
Marek Michali <marek.michali@sap.com>
2023-11-17 12:05:45 +0100

    Refactor check for artifacts

--- scripts/check_artifacts_existence.sh
+++ scripts/check_artifacts_existence.sh
@@ -9,58 +9,25 @@ set -o pipefail # prevents errors in a pipeline from being masked
 # This script has the following arguments:
 #                       -  image tag - mandatory
 #
-# ./check_artifacts_existence.sh v2.1.0
+# ./check_artifacts_existence.sh 2.1.0
 
 
 export IMAGE_TAG=$1
 
 PROTOCOL=docker://
+IMAGE_NAMES=(
+  kyma-environment-broker
+  kyma-environment-deprovision-retrigger-job
+  kyma-environment-runtime-reconciler
+  kyma-environment-subaccount-cleanup-job
+  kyma-environment-trial-cleanup-job
+  kyma-environments-cleanup-job
+)
 
-if [ $(skopeo list-tags ${PROTOCOL}europe-docker.pkg.dev/kyma-project/prod/kyma-environment-broker | jq '.Tags|any(. == env.IMAGE_TAG)') == "true" ]
-then
-  echo "::warning ::Kyma Environment Broker binary image for tag ${IMAGE_TAG} already exists"
-else
-  echo "No previous Kyma Environment Broker binary image found for tag ${IMAGE_TAG}"
-fi
-
-if [ $(skopeo list-tags ${PROTOCOL}europe-docker.pkg.dev/kyma-project/prod/kyma-environment-deprovision-retrigger-job | jq '.Tags|any(. == env.IMAGE_TAG)') == "true" ]
-then
-  echo "::warning ::Kyma Environment deprovision retrigger job binary image for tag ${IMAGE_TAG} already exists"
-else
-  echo "No previous Kyma Environment deprovision retrigger job binary image found for tag ${IMAGE_TAG}"
-fi
-
-if [ $(skopeo list-tags ${PROTOCOL}europe-docker.pkg.dev/kyma-project/prod/kyma-environment-runtime-reconciler | jq '.Tags|any(. == env.IMAGE_TAG)') == "true" ]
-then
-  echo "::warning ::Kyma Environment runtime reconciler binary image for tag ${IMAGE_TAG} already exists"
-else
-  echo "No previous Kyma Environment runtime reconciler binary image found for tag ${IMAGE_TAG}"
-fi
-
-if [ $(skopeo list-tags ${PROTOCOL}europe-docker.pkg.dev/kyma-project/prod/kyma-environment-subaccount-cleanup-job | jq '.Tags|any(. == env.IMAGE_TAG)') == "true" ]
-then
-  echo "::warning ::Kyma Environment subaccount cleanup job binary image for tag ${IMAGE_TAG} already exists"
-else
-  echo "No previous Kyma Environment subaccount cleanup job binary image found for tag ${IMAGE_TAG}"
-fi
-
-if [ $(skopeo list-tags ${PROTOCOL}europe-docker.pkg.dev/kyma-project/prod/kyma-environment-subscription-cleanup-job | jq '.Tags|any(. == env.IMAGE_TAG)') == "true" ]
-then
-  echo "::warning ::Kyma Environment subscription cleanup job binary image for tag ${IMAGE_TAG} already exists"
-else
-  echo "No previous Kyma Environment subscription cleanup job binary image found for tag ${IMAGE_TAG}"
-fi
-
-if [ $(skopeo list-tags ${PROTOCOL}europe-docker.pkg.dev/kyma-project/prod/kyma-environment-trial-cleanup-job | jq '.Tags|any(. == env.IMAGE_TAG)') == "true" ]
-then
-  echo "::warning ::Kyma Environment trial cleanup job binary image for tag ${IMAGE_TAG} already exists"
-else
-  echo "No previous Kyma Environment trial cleanup job binary image found for tag ${IMAGE_TAG}"
-fi
-
-if [ $(skopeo list-tags ${PROTOCOL}europe-docker.pkg.dev/kyma-project/prod/kyma-environments-cleanup-job | jq '.Tags|any(. == env.IMAGE_TAG)') == "true" ]
-then
-  echo "::warning ::Kyma Environments cleanup job binary image for tag ${IMAGE_TAG} already exists"
-else
-  echo "No previous Kyma Environments cleanup job binary image found for tag ${IMAGE_TAG}"
-fi
+for image in "${IMAGE_NAMES[@]}"; do
+  if [ $(skopeo list-tags ${PROTOCOL}europe-docker.pkg.dev/kyma-project/prod/${image} | jq '.Tags|any(. == env.IMAGE_TAG)') == "true" ]; then
+    echo "::warning ::${image} binary image for tag ${IMAGE_TAG} already exists"
+  else
+    echo "No previous ${image} binary image found for tag ${IMAGE_TAG}"
+  fi
+done
