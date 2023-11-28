fa2327d531bf3c1d93d8969ae2244f84cc25ecfa
kyma-btp-manager-serviceuser <jaroslaw.pieszka@sap.com>
2023-11-28 17:31:20 +0100

    make imports

--- internal/process/provisioning/resolve_creds.go
+++ internal/process/provisioning/resolve_creds.go
@@ -2,8 +2,9 @@ package provisioning
 
 import (
 	"fmt"
-	"github.com/kyma-project/kyma-environment-broker/internal/provider"
 	"time"
+
+	"github.com/kyma-project/kyma-environment-broker/internal/provider"
 
 	"github.com/kyma-project/kyma-environment-broker/common/hyperscaler"
 	"github.com/kyma-project/kyma-environment-broker/internal"
