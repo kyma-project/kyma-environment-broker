3a1b08fe60f50f07c8d8a5fcd13c69e7cdae9164
Marcin Szwed <marcin.szwed@sap.com>
2023-11-30 20:36:14 +0100

    Define test steps

--- testing/e2e/skr/trial-suspension-test/test.js
+++ testing/e2e/skr/trial-suspension-test/test.js
@@ -1,35 +1,41 @@
+const {KCPWrapper, KCPConfig} = require('../kcp/client');
+const {KEBClient, KEBConfig} = require("../kyma-environment-broker");
 const {gatherOptions} = require("../skr-test/helpers");
 const {getOrProvisionSKR} = require("../skr-test/provision/provision-skr");
-const {KCPWrapper, KCPConfig} = require('../kcp/client');
+const {ensureOperationSucceeded} = require("../kyma-environment-broker/helpers");
 const {deprovisionAndUnregisterSKR} = require("../skr-test/provision/deprovision-skr");
 
 const kcp = new KCPWrapper(KCPConfig.fromEnv());
+const keb = new KEBClient(KEBConfig.fromEnv());
 
 const provisioningTimeout = 1000 * 60 * 30; // 30m
 const suspensionTimeout = 1000 * 60 * 60; // 60m
 const deprovisioningAfterSuspensionTimeout = 1000 * 60 * 5; // 5m
+const trialCleanupTriggerTimeout = 1000 * 60 * 11; // 11m
 const slowTestDuration = 1000 * 60 * 40; // 40m
 const globalTimeout = provisioningTimeout + suspensionTimeout;
+const suspensionOperationType = "suspension"
+const inProgressOperationState = "in progress"
 
 describe('SKR Trial suspension test', function () {
     this.timeout(globalTimeout);
     this.slow(slowTestDuration);
 
     let skr;
+    let suspensionOpID;
     let options = gatherOptions();
 
     before('Ensure SKR Trial is provisioned', async function() {
-        this.timeout(provisioningTimeout);
         skr = await getOrProvisionSKR(options, false, provisioningTimeout);
-        options = skr.options;
     });
 
     it('should wait until Trial Cleanup CronJob triggers suspension', async function() {
-
+        const rs = await kcp.ensureLatestGivenOperationTypeIsInGivenState(options.instanceID, suspensionOperationType, inProgressOperationState, trialCleanupTriggerTimeout);
+        suspensionOpID = rs.data[0].status[suspensionOperationType].data[0].operationID;
     });
 
     it('should wait until suspension succeeds', async function() {
-
+        await ensureOperationSucceeded(keb, kcp, options.instanceID, suspensionOpID, suspensionTimeout);
     });
 
     after('Cleanup the resources', async function() {
