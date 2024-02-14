b5ce6750fc36be6d1b3df0bdedac26a2db6b871e
Marek Michali <marek.michali@sap.com>
2024-02-14 08:30:51 +0100

    Test

--- envtest.sh
+++ envtest.sh
@@ -6,7 +6,7 @@ mkdir -p "$LOCAL_BIN"
 
 K8S_VERSION=1.29.1
 
-#check if setup-envtest is installed or check if currently installed setup-envtest contains assets for requested k8s version
+#check if setup-envtest is installed or check if currently installed setup-envtest contains assets for requested k8s version test
 if [ ! -e "$LOCAL_BIN/setup-envtest" ] || [ -z $("$LOCAL_BIN"/setup-envtest list -i  | awk '{print $2}' | tr -d v | grep "$K8S_VERSION") ]; then
   GOBIN="$LOCAL_BIN" go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
   if [ $? -ne 0 ]; then
