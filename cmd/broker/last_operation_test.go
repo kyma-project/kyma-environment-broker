f0ede84a0d464d9785529acfea09de9968d29c4d
Ksawery Zietara <Ksawery.Zietara@sap.com>
2024-02-19 12:11:11 +0100

    Change operation logs

--- cmd/broker/last_operation_test.go
+++ cmd/broker/last_operation_test.go
@@ -91,7 +91,7 @@ func TestLastOperationNotExistingInstance(t *testing.T) {
 	//then
 	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
 	errResponse := suite.DecodeErrorResponse(resp)
-	assert.Regexp(t, fmt.Sprintf("(?i)operation with instance_id %s not (found|exist)", iid), errResponse.Description)
+	assert.Contains(t, errResponse.Description, fmt.Sprintf("Operation with instance_id %s not exist", iid))
 }
 
 func TestLastOperationNotExistingOperation(t *testing.T) {
@@ -107,7 +107,7 @@ func TestLastOperationNotExistingOperation(t *testing.T) {
 	//then
 	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
 	errResponse := suite.DecodeErrorResponse(resp)
-	assert.Regexp(t, fmt.Sprintf("(?i)operation with id %s not (found|exist)", oid), errResponse.Description)
+	assert.Contains(t, errResponse.Description, fmt.Sprintf("Operation with id %s not exist", oid))
 }
 
 func TestLastOperationWithOperationIDAndNotExistingInstanceID(t *testing.T) {
