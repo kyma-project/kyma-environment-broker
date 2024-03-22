4c48d5eca174afbe651d9bd8d108a73d07b99620
Marcin Szwed <marcin.szwed@sap.com>
2024-03-22 12:18:34 +0100

    Add events endpoint

--- internal/cis/fake_server.go
+++ internal/cis/fake_server.go
@@ -7,13 +7,18 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strconv"
+	"time"
 )
 
 const (
 	FakeSubaccountID1   = "cad2806a-3545-4aa0-8a7c-4fc246dba684"
 	FakeSubaccountID2   = "17b8dcc2-3de1-4884-bcd3-b1c4657d81be"
+	eventsJSONPath      = "testdata/events.json"
 	subaccountsJSONPath = "testdata/subaccounts.json"
 	subaccountIDJSONKey = "guid"
+	eventTypeJSONKey    = "eventType"
+	actionTimeJSONKey   = "actionTime"
 )
 
 type fakeServer struct {
@@ -24,6 +29,12 @@ type fakeServer struct {
 type subaccountsEndpoint struct {
 	subaccounts map[string]map[string]interface{}
 }
+
+type eventsEndpoint struct {
+	events []map[string]interface{}
+}
+
+type mutableEvents []map[string]interface{}
 
 func NewFakeServer() *fakeServer {
 	se := newSubaccountsEndpoint()
@@ -91,4 +102,90 @@ func (e *subaccountsEndpoint) getSubaccount(w http.ResponseWriter, r *http.Reque
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(data)
+}
+
+func newEventsEndpoint() *eventsEndpoint {
+	endpoint := &eventsEndpoint{events: make([]map[string]interface{}, 0)}
+
+	f, err := os.Open(eventsJSONPath)
+	defer f.Close()
+	if err != nil {
+		log.Fatal(fmt.Errorf("while reading events JSON file: %w", err))
+	}
+
+	d := json.NewDecoder(f)
+	err = d.Decode(&endpoint.events)
+	if err != nil {
+		log.Fatal(fmt.Errorf("while decoding events JSON: %w", err))
+	}
+
+	return endpoint
+}
+
+func (e *eventsEndpoint) getEvents(w http.ResponseWriter, r *http.Request) {
+	events := make(mutableEvents, len(e.events))
+	events = append(events, e.events...)
+
+	query := r.URL.Query()
+	eventTypeFilter := query.Get("eventType")
+	actionTimeFilter := query.Get("fromActionTime")
+
+	if eventTypeFilter != "" {
+		events.filterEventsByEventType(eventTypeFilter)
+	}
+	if actionTimeFilter != "" {
+		events.filterEventsByActionTime(actionTimeFilter)
+	}
+
+	data, err := json.Marshal(events)
+	if err != nil {
+		log.Fatal(fmt.Errorf("while marshalling events data: %w", err))
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
+
+func (e *mutableEvents) filterEventsByEventType(eventTypeFilter string) {
+	for i, event := range *e {
+		ival, ok := event[eventTypeJSONKey]
+		if !ok {
+			log.Println("missing eventType key in one of events")
+			continue
+		}
+		actualEventType, ok := ival.(string)
+		if !ok {
+			log.Println("cannot cast eventType value to string - wrong value in one of events")
+			continue
+		}
+		if actualEventType != eventTypeFilter {
+			*e = append((*e)[:i], (*e)[i+1:]...)
+		}
+	}
+}
+
+func (e *mutableEvents) filterEventsByActionTime(actionTimeFilter string) {
+	filterInUnixMilli, err := strconv.ParseInt(actionTimeFilter, 10, 64)
+	if err != nil {
+		log.Println("cannot parse actionTime filter to int64")
+		return
+	}
+
+	timeFilter := time.UnixMilli(filterInUnixMilli)
+	for i, event := range *e {
+		ival, ok := event[actionTimeJSONKey]
+		if !ok {
+			log.Println("missing actionTime key in one of events")
+			continue
+		}
+		actualActionTimeInUnixMilli, ok := ival.(int64)
+		if !ok {
+			log.Println("cannot cast actionTime value to int64 - wrong value in one of events")
+			continue
+		}
+		actualActionTime := time.UnixMilli(actualActionTimeInUnixMilli)
+		if actualActionTime.Before(timeFilter) {
+			*e = append((*e)[:i], (*e)[i+1:]...)
+		}
+	}
 }
