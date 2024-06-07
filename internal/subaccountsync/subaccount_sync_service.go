395ce9db1b623b5d4a20d0f051045d113bfb4959
Pieszka <jaroslaw.pieszka@sap.com>
2024-06-07 13:52:09 +0200

    go import

--- internal/subaccountsync/subaccount_sync_service.go
+++ internal/subaccountsync/subaccount_sync_service.go
@@ -3,11 +3,12 @@ package subaccountsync
 import (
 	"context"
 	"fmt"
-	"github.com/kyma-project/kyma-environment-broker/internal/storage/dbmodel"
 	"log/slog"
 	"net/http"
 	"sync"
 	"time"
+
+	"github.com/kyma-project/kyma-environment-broker/internal/storage/dbmodel"
 
 	"github.com/kyma-project/kyma-environment-broker/internal/kymacustomresource"
 	"github.com/kyma-project/kyma-environment-broker/internal/storage"
