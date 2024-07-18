2f2a8bf99161dcff696f2dafbd12d4aef85acd1b
Pieszka <jaroslaw.pieszka@sap.com>
2024-07-18 13:56:20 +0200

    no sleep

--- cmd/broker/deprovisioning_test.go
+++ cmd/broker/deprovisioning_test.go
@@ -3,12 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/stretchr/testify/require"
 	"io"
 	"net/http"
 	"testing"
-	"time"
-
-	"github.com/stretchr/testify/require"
 
 	"github.com/google/uuid"
 	"github.com/kyma-project/kyma-environment-broker/common/runtime"
@@ -197,8 +195,6 @@ func TestRuntimesEndpointForDeprovisionedInstance(t *testing.T) {
 
 	suite.FinishDeprovisioningOperationByProvisioner(depOpID)
 	suite.WaitForOperationsNotExists(iid1) // deprovisioning completed, no operations in the DB
-
-	time.Sleep(1 * time.Second) // to make instances differ in respect to timestamp
 
 	iid2 := uuid.New().String()
 	resp = suite.CallAPI("PUT", fmt.Sprintf("oauth/cf-eu10/v2/service_instances/%s?accepts_incomplete=true&plan_id=b1a5764e-2ea1-4f95-94c0-2b4538b37b55&service_id=47c9dcbf-ff30-448e-ab36-d3bad66ba281", iid2),
