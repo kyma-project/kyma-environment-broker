13649a38b1f1b870a31b3bd259e4bf6c43ae85c6
Pieszka <jaroslaw.pieszka@sap.com>
2024-07-18 07:58:29 +0200

    review remarks

--- internal/process/provisioning/create_kyma_name_step.go
+++ internal/process/provisioning/create_kyma_name_step.go
@@ -2,9 +2,8 @@ package provisioning
 
 import (
 	"fmt"
-	"time"
-
 	"github.com/kyma-project/kyma-environment-broker/internal/process/steps"
+	"time"
 
 	"github.com/kyma-project/kyma-environment-broker/internal"
 	"github.com/kyma-project/kyma-environment-broker/internal/process"
@@ -31,9 +30,7 @@ func (s *CreateKymaNameStep) Run(operation internal.Operation, log logrus.FieldL
 		return s.operationManager.OperationFailed(operation, fmt.Sprint("RuntimeID not set, cannot create Kyma name"), nil, log)
 	}
 
-	operation.KymaResourceName = steps.CreateKymaNameFromOperation(operation)
-
 	return s.operationManager.UpdateOperation(operation, func(op *internal.Operation) {
-		op.RuntimeID = operation.RuntimeID
+		operation.KymaResourceName = steps.CreateKymaNameFromOperation(operation)
 	}, log)
 }
