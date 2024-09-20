32faad711c201c574b4de1fda1d10c1a8055c1db
Pieszka <jaroslaw.pieszka@sap.com>
2024-09-20 10:20:58 +0200

    almost

--- envtest.sh
+++ envtest.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# standard bash error handling
 set -o nounset  # treat unset variables as an error and exit immediately.
 set -o errexit  # exit immediately when a command fails.
-set -E          # needs to be set if we want the ERR trap
 
 cd "$(dirname "$0")" || exit
 
