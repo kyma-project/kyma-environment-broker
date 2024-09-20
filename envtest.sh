4630c6ea51019a9db53acc1e9347d9f69302654c
Pieszka <jaroslaw.pieszka@sap.com>
2024-09-20 10:19:56 +0200

    almost standard error handling

--- envtest.sh
+++ envtest.sh
@@ -4,7 +4,6 @@
 set -o nounset  # treat unset variables as an error and exit immediately.
 set -o errexit  # exit immediately when a command fails.
 set -E          # needs to be set if we want the ERR trap
-set -o pipefail # prevents errors in a pipeline from being masked
 
 cd "$(dirname "$0")" || exit
 
