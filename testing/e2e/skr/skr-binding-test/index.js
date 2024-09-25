c8bbbf49de36019092b9c5af22066b629b8aacfc
Marek Michali <marek.michali@sap.com>
2024-09-25 09:18:49 +0200

    Add skr binding test

---
+++ testing/e2e/skr/skr-binding-test/index.js
@@ -0,0 +1,51 @@
+const {
+  gatherOptions,
+} = require('../skr-test');
+const {
+  initializeK8sClient,
+} = require('../utils/index.js');
+const {provisionSKRAndInitK8sConfig} = require('../skr-test/provision/provision-skr');
+const {deprovisionAndUnregisterSKR} = require('../skr-test/provision/deprovision-skr');
+const {KEBClient, KEBConfig} = require('../kyma-environment-broker');
+const keb = new KEBClient(KEBConfig.fromEnv());
+
+const provisioningTimeout = 1000 * 60 * 30; // 30m
+const deprovisioningTimeout = 1000 * 60 * 95; // 95m
+let globalTimeout = 1000 * 60 * 70; // 70m
+const slowTime = 5000;
+
+describe('SKR test', function() {
+  globalTimeout += provisioningTimeout + deprovisioningTimeout;
+
+  this.timeout(globalTimeout);
+  this.slow(slowTime);
+
+  let options = gatherOptions(); // with default values
+  let skr;
+  let kubeconfigFromBinding;
+
+  before('Ensure SKR is provisioned', async function() {
+    this.timeout(provisioningTimeout);
+    skr = await provisionSKRAndInitK8sConfig(options, provisioningTimeout);
+    options = skr.options;
+  });
+
+  it('Create SKR binding', async function() {
+    try {
+      kubeconfigFromBinding = await keb.createBinding(options.instanceID)
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  it('Initiate K8s client with kubeconfig from binding', async function() {
+      await initializeK8sClient({kubeconfig: kubeconfig});
+  });
+  
+  after('Cleanup the resources', async function() {
+    this.timeout(deprovisioningTimeout);
+    if (process.env['SKIP_DEPROVISIONING'] != 'true') {
+      await deprovisionAndUnregisterSKR(options, deprovisioningTimeout, true);
+    }
+  });
+});
