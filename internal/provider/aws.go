95f7110e5c784d5e6502497e7cafd827f14f10a9
Pieszka <jaroslaw.pieszka@sap.com>
2024-09-30 13:34:33 +0200

    sap cc default test case added
    corrected logic for zones
    
    panic en route
    
    still panic but simpler code
    
    TODOs added
    
    tests for GCP

--- internal/provider/aws.go
+++ internal/provider/aws.go
@@ -86,10 +86,12 @@ func (p *AWSTrialInputProvider) region() string {
 	if p.ProvisioningParameters.PlatformRegion != "" {
 		abstractRegion, found := p.PlatformRegionMapping[p.ProvisioningParameters.PlatformRegion]
 		if found {
+			//TODO safe check
 			return *toAWSSpecific[abstractRegion]
 		}
 	}
 	if p.ProvisioningParameters.Parameters.Region != nil && *p.ProvisioningParameters.Parameters.Region != "" {
+		// TODO safe check
 		return *toAWSSpecific[*p.ProvisioningParameters.Parameters.Region]
 	}
 	return DefaultAWSTrialRegion
