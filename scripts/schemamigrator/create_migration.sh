2d952a2cbb463fdd66ad2e8896b978df8fa47c82
Marcin Szwed <marcin.szwed@sap.com>
2024-10-02 11:51:11 +0200

    Adjust create_migration.sh

--- scripts/schemamigrator/create_migration.sh
+++ scripts/schemamigrator/create_migration.sh
@@ -2,22 +2,16 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-COMPONENT=$1
-NAME=$2
+NAME=$1
 
-for var in COMPONENT NAME; do
-    if [ -z "${!var}" ] ; then
-        echo "One or more arguments not provided. Usage: ./create_migration [COMPONENT] [NAME]"
-        exit 1
-    fi
-done
+if [ -z "$NAME" ] ; then
+    echo "NAME argument not provided. Usage: ./create_migration [NAME]"
+    exit 1
+fi
 
 DATE="$(date +%Y%m%d%H%M)"
-MIGRATIONS_DIR="../../resources/kcp/charts/${COMPONENT}/migrations"
+MIGRATIONS_DIR="../../resources/migrations"
 TRANSACTION_STR=$'BEGIN;\nCOMMIT;'
-if [ $COMPONENT == "kyma-environment-broker" ] || [ $COMPONENT == "provisioner" ] ; then
-    mkdir -p ${MIGRATIONS_DIR}
-fi
 
 echo "$TRANSACTION_STR" > "${MIGRATIONS_DIR}/${DATE}_${NAME}.up.sql"
 echo "$TRANSACTION_STR" > "${MIGRATIONS_DIR}/${DATE}_${NAME}.down.sql"
