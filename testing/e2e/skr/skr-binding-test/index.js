c7afb2a22e1d6ff30783d7c48fb1f64638150b7c
Marek Michali <marek.michali@sap.com>
2024-10-04 15:10:37 +0200

    Change message

--- testing/e2e/skr/skr-binding-test/index.js
+++ testing/e2e/skr/skr-binding-test/index.js
@@ -62,7 +62,7 @@ describe('SKR Binding test', function() {
     await getSecret(secretName, ns);
   });
 */
-  it('Should fail creation of SKR binding when expiration seconds value is below the minimum value', async function() {
+  it('Should not allow creation of SKR binding when expiration seconds value is below the minimum value', async function() {
     const expirationSeconds = 10;
     try {
       kubeconfigFromBinding = await keb.createBinding("4D0CE8A5-714F-45E1-9B17-FE34FEEFF44B", true, expirationSeconds);
