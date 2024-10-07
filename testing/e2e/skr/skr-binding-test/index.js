dcfdf1a6b7224435b909e9c60ac4495a8f747d5b
Marek Michali <marek.michali@sap.com>
2024-10-07 11:48:49 +0200

    Logs

--- testing/e2e/skr/skr-binding-test/index.js
+++ testing/e2e/skr/skr-binding-test/index.js
@@ -81,7 +81,7 @@ describe('SKR Binding test', function() {
     try {
       await keb.createBinding2("0EFB3BD5-EDA1-4659-AA18-597236230931", true, expirationSeconds);
       console.log("The test was expected to fail but it passed");
-      expect.fail("The test was expected to fail but it passed");
+      assert.fail("The test was expected to fail but it passed")
     } catch (err) {
       if (err.response) {
         expect(err.response.status).equal(400);
@@ -89,7 +89,7 @@ describe('SKR Binding test', function() {
         console.log('Got response:');
         console.log(err.response.data);
       } else {
-        console.log(err);
+        console.log('Error response not available');
       }
     
     }
