2af22f11c9cd85af5f54c5c9daa8e4ca69854a5b
Marek Michali <marek.michali@sap.com>
2024-10-07 09:12:19 +0200

    Increase timeout

--- testing/e2e/skr/skr-binding-test/index.js
+++ testing/e2e/skr/skr-binding-test/index.js
@@ -65,14 +65,14 @@ describe('SKR Binding test', function() {
   it('Should not allow creation of SKR binding when expiration seconds value is below the minimum value', async function() {
     const expirationSeconds = 700;
 
-    expect(function () { keb.createBinding("0EFB3BD5-EDA1-4659-AA18-597236230931", true, expirationSeconds).timeout(5000); }).to.throw();
+    expect(function () { keb.createBinding("0EFB3BD5-EDA1-4659-AA18-597236230931", true, expirationSeconds).timeout(10000); }).to.throw();
       
   });
 
   it('Should not allow creation of SKR binding when expiration seconds value is over the maximum value', async function() {
     const expirationSeconds = 700;
     try {
-      kubeconfigFromBinding = await keb.createBinding("0EFB3BD5-EDA1-4659-AA18-597236230931", true, expirationSeconds).timeout(5000);
+      kubeconfigFromBinding = await keb.createBinding("0EFB3BD5-EDA1-4659-AA18-597236230931", true, expirationSeconds).timeout(10000);
       console.log("The test was expected to fail but it passed");
       expect.fail();
     } catch (err) { }
