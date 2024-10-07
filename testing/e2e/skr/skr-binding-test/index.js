3a82ee24145b6f49ba27ac10163352b868dfc4b8
Marek Michali <marek.michali@sap.com>
2024-10-07 09:42:20 +0200

    Tiemouts

--- testing/e2e/skr/skr-binding-test/index.js
+++ testing/e2e/skr/skr-binding-test/index.js
@@ -63,7 +63,7 @@ describe('SKR Binding test', function() {
   });
 
   it('Should not allow creation of SKR binding when expiration seconds value is below the minimum value', async function() {
-    const expirationSeconds = 700;
+    const expirationSeconds = 1;
     this.timeout(10000);
 
     await expect(function () { keb.createBinding("0EFB3BD5-EDA1-4659-AA18-597236230931", true, expirationSeconds); }).to.throw();
