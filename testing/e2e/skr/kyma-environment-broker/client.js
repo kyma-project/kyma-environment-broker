d85997b9398802206b3c0e15a942e5f5d588f8c3
Marek Michali <marek.michali@sap.com>
2024-10-21 13:56:53 +0200

    Refactor err handling

--- testing/e2e/skr/kyma-environment-broker/client.js
+++ testing/e2e/skr/kyma-environment-broker/client.js
@@ -283,35 +283,21 @@ class KEBClient {
     };
     const endpoint = `service_instances/${instanceID}/service_bindings/${bindingID}?accepts_incomplete=false`;
     const config = await this.buildRequest(payload, endpoint, 'put');
+    return await axios.request(config);
 
-    try {
-      return await axios.request(config);
-    } catch (err) {
-      throw err;
-    }
   }
 
   async deleteBinding(instanceID, bindingID) {
     const params = `service_id=${KYMA_SERVICE_ID}&plan_id=${this.planID}`;
     const endpoint = `service_instances/${instanceID}/service_bindings/${bindingID}?accepts_incomplete=false&${params}`;
     const config = await this.buildRequest({}, endpoint, 'delete');
-
-    try {
-      return await axios.request(config);
-    } catch (err) {
-      throw err;
-    }
+    return await axios.request(config);
   }
 
   async getBinding(instanceID, bindingID) {
     const endpoint = `service_instances/${instanceID}/service_bindings/${bindingID}?accepts_incomplete=false`;
     const config = await this.buildRequest({}, endpoint, 'get');
-
-    try {
-      return await axios.request(config);
-    } catch (err) {
-      throw err;
-    }
+    return await axios.request(config);
   }
 
   getPlatformRegion() {
