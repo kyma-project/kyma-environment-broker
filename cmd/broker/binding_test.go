47f7831ee357bd2b402e5d0766cc28a573f548c0
Pieszka <jaroslaw.pieszka@sap.com>
2024-10-23 13:24:30 +0200

    http method enum used

--- cmd/broker/binding_test.go
+++ cmd/broker/binding_test.go
@@ -21,7 +21,7 @@ func TestBinding(t *testing.T) {
 	iid := uuid.New().String()
 	bid := uuid.New().String()
 
-	resp := suite.CallAPI("PUT", fmt.Sprintf("oauth/v2/service_instances/%s?accepts_incomplete=true", iid),
+	resp := suite.CallAPI(http.MethodPut, fmt.Sprintf("oauth/v2/service_instances/%s?accepts_incomplete=true", iid),
 		`{
 					"service_id": "47c9dcbf-ff30-448e-ab36-d3bad66ba281",
 					"plan_id": "361c511f-f939-4621-b228-d0fb79a1fe15",
@@ -40,7 +40,7 @@ func TestBinding(t *testing.T) {
 	suite.WaitForOperationState(opID, domain.Succeeded)
 
 	// when
-	resp = suite.CallAPI("PUT", fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s", iid, bid),
+	resp = suite.CallAPI(http.MethodPut, fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s", iid, bid),
 		`{
                 "service_id": "47c9dcbf-ff30-448e-ab36-d3bad66ba281",
                 "plan_id": "361c511f-f939-4621-b228-d0fb79a1fe15"
@@ -50,13 +50,13 @@ func TestBinding(t *testing.T) {
 	suite.Log(string(b))
 	suite.Log(resp.Status)
 
-	respRuntimes := suite.CallAPI("GET", "/runtimes?bindings=true", "")
+	respRuntimes := suite.CallAPI(http.MethodGet, "/runtimes?bindings=true", "")
 	b, _ = io.ReadAll(respRuntimes.Body)
 	suite.Log(string(b))
 	suite.Log(resp.Status)
 
 	t.Run("should return 400 when expiration seconds parameter is string instead of int", func(t *testing.T) {
-		resp = suite.CallAPI("PUT", fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s", iid, bid),
+		resp = suite.CallAPI(http.MethodPut, fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s", iid, bid),
 			`{
                 "service_id": "47c9dcbf-ff30-448e-ab36-d3bad66ba281",
                 "plan_id": "361c511f-f939-4621-b228-d0fb79a1fe15",
@@ -69,7 +69,7 @@ func TestBinding(t *testing.T) {
 
 	t.Run("should return 200 when creating a second binding with the same id and params as an existing one", func(t *testing.T) {
 		bid = uuid.New().String()
-		resp = suite.CallAPI("PUT", fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s", iid, bid),
+		resp = suite.CallAPI(http.MethodPut, fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s", iid, bid),
 			`{
                 "service_id": "47c9dcbf-ff30-448e-ab36-d3bad66ba281",
                 "plan_id": "361c511f-f939-4621-b228-d0fb79a1fe15",
@@ -77,7 +77,7 @@ func TestBinding(t *testing.T) {
 					"expiration_seconds": 600
 				}
                }`)
-		resp = suite.CallAPI("PUT", fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s", iid, bid),
+		resp = suite.CallAPI(http.MethodPut, fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s", iid, bid),
 			`{
                 "service_id": "47c9dcbf-ff30-448e-ab36-d3bad66ba281",
                 "plan_id": "361c511f-f939-4621-b228-d0fb79a1fe15",
@@ -88,7 +88,7 @@ func TestBinding(t *testing.T) {
 		assert.Equal(t, http.StatusOK, resp.StatusCode)
 
 		// "expiration_seconds": 600 is a default value from the config in tests
-		resp = suite.CallAPI("PUT", fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s", iid, bid),
+		resp = suite.CallAPI(http.MethodPut, fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s", iid, bid),
 			`{
                 "service_id": "47c9dcbf-ff30-448e-ab36-d3bad66ba281",
                 "plan_id": "361c511f-f939-4621-b228-d0fb79a1fe15"
@@ -98,7 +98,7 @@ func TestBinding(t *testing.T) {
 
 	t.Run("should return 409 when creating a second binding with the same id as an existing one but different params", func(t *testing.T) {
 		bid = uuid.New().String()
-		resp = suite.CallAPI("PUT", fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s", iid, bid),
+		resp = suite.CallAPI(http.MethodPut, fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s", iid, bid),
 			`{
                 "service_id": "47c9dcbf-ff30-448e-ab36-d3bad66ba281",
                 "plan_id": "361c511f-f939-4621-b228-d0fb79a1fe15",
@@ -106,7 +106,7 @@ func TestBinding(t *testing.T) {
 					"expiration_seconds": 900
 				}
                }`)
-		resp = suite.CallAPI("PUT", fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s", iid, bid),
+		resp = suite.CallAPI(http.MethodPut, fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s", iid, bid),
 			`{
                 "service_id": "47c9dcbf-ff30-448e-ab36-d3bad66ba281",
                 "plan_id": "361c511f-f939-4621-b228-d0fb79a1fe15",
@@ -131,7 +131,7 @@ func TestDeprovisioningWithExistingBindings(t *testing.T) {
 	bindingID1 := uuid.New().String()
 	bindingID2 := uuid.New().String()
 
-	response := suite.CallAPI("PUT", fmt.Sprintf("oauth/v2/service_instances/%s?accepts_incomplete=true", iid),
+	response := suite.CallAPI(http.MethodPut, fmt.Sprintf("oauth/v2/service_instances/%s?accepts_incomplete=true", iid),
 		`{
 					"service_id": "47c9dcbf-ff30-448e-ab36-d3bad66ba281",
 					"plan_id": "361c511f-f939-4621-b228-d0fb79a1fe15",
@@ -150,14 +150,14 @@ func TestDeprovisioningWithExistingBindings(t *testing.T) {
 	suite.WaitForOperationState(opID, domain.Succeeded)
 
 	// when we create two bindings
-	response = suite.CallAPI("PUT", fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s", iid, bindingID1),
+	response = suite.CallAPI(http.MethodPut, fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s", iid, bindingID1),
 		`{
                 "service_id": "47c9dcbf-ff30-448e-ab36-d3bad66ba281",
                 "plan_id": "361c511f-f939-4621-b228-d0fb79a1fe15"
                }`)
 	require.Equal(t, http.StatusCreated, response.StatusCode)
 
-	response = suite.CallAPI("PUT", fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s", iid, bindingID2),
+	response = suite.CallAPI(http.MethodPut, fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s", iid, bindingID2),
 		`{
                 "service_id": "47c9dcbf-ff30-448e-ab36-d3bad66ba281",
                 "plan_id": "361c511f-f939-4621-b228-d0fb79a1fe15"
@@ -165,16 +165,16 @@ func TestDeprovisioningWithExistingBindings(t *testing.T) {
 	require.Equal(t, http.StatusCreated, response.StatusCode)
 
 	// when we deprovision successfully
-	response = suite.CallAPI("DELETE", fmt.Sprintf("oauth/v2/service_instances/%s?accepts_incomplete=true&plan_id=361c511f-f939-4621-b228-d0fb79a1fe15&service_id=47c9dcbf-ff30-448e-ab36-d3bad66ba281", iid),
+	response = suite.CallAPI(http.MethodDelete, fmt.Sprintf("oauth/v2/service_instances/%s?accepts_incomplete=true&plan_id=361c511f-f939-4621-b228-d0fb79a1fe15&service_id=47c9dcbf-ff30-448e-ab36-d3bad66ba281", iid),
 		``)
 	deprovisioningID := suite.DecodeOperationID(response)
 	suite.FinishDeprovisioningOperationByProvisioner(deprovisioningID)
 	suite.WaitForInstanceRemoval(iid)
 
 	// when we remove bindings and the instance is already removed
-	response = suite.CallAPI("DELETE", fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s?plan_id=361c511f-f939-4621-b228-d0fb79a1fe15&service_id=47c9dcbf-ff30-448e-ab36-d3bad66ba281", iid, bindingID1), "")
+	response = suite.CallAPI(http.MethodDelete, fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s?plan_id=361c511f-f939-4621-b228-d0fb79a1fe15&service_id=47c9dcbf-ff30-448e-ab36-d3bad66ba281", iid, bindingID1), "")
 	assert.Equal(t, http.StatusGone, response.StatusCode)
-	response = suite.CallAPI("DELETE", fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s?plan_id=361c511f-f939-4621-b228-d0fb79a1fe15&service_id=47c9dcbf-ff30-448e-ab36-d3bad66ba281", iid, bindingID2), "")
+	response = suite.CallAPI(http.MethodDelete, fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s?plan_id=361c511f-f939-4621-b228-d0fb79a1fe15&service_id=47c9dcbf-ff30-448e-ab36-d3bad66ba281", iid, bindingID2), "")
 	assert.Equal(t, http.StatusGone, response.StatusCode)
 
 	// then expect bindings to be removed
@@ -192,7 +192,7 @@ func TestRemoveBindingsFromSuspended(t *testing.T) {
 	bindingID1 := uuid.New().String()
 	bindingID2 := uuid.New().String()
 
-	response := suite.CallAPI("PUT", fmt.Sprintf("oauth/cf-eu10/v2/service_instances/%s?accepts_incomplete=true&plan_id=7d55d31d-35ae-4438-bf13-6ffdfa107d9f&service_id=47c9dcbf-ff30-448e-ab36-d3bad66ba281", iid),
+	response := suite.CallAPI(http.MethodPut, fmt.Sprintf("oauth/cf-eu10/v2/service_instances/%s?accepts_incomplete=true&plan_id=7d55d31d-35ae-4438-bf13-6ffdfa107d9f&service_id=47c9dcbf-ff30-448e-ab36-d3bad66ba281", iid),
 		`{
 					"service_id": "47c9dcbf-ff30-448e-ab36-d3bad66ba281",
 					"plan_id": "7d55d31d-35ae-4438-bf13-6ffdfa107d9f",
@@ -210,7 +210,7 @@ func TestRemoveBindingsFromSuspended(t *testing.T) {
 	suite.WaitForOperationState(opID, domain.Succeeded)
 
 	//then we create two bindings
-	response = suite.CallAPI("PUT", fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s", iid, bindingID1),
+	response = suite.CallAPI(http.MethodPut, fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s", iid, bindingID1),
 		`{
 	           "service_id": "47c9dcbf-ff30-448e-ab36-d3bad66ba281",
 	           "plan_id": "7d55d31d-35ae-4438-bf13-6ffdfa107d9f"
@@ -225,7 +225,7 @@ func TestRemoveBindingsFromSuspended(t *testing.T) {
 	require.Equal(t, http.StatusCreated, response.StatusCode)
 
 	//when we suspend Service instance - OSB context update
-	response = suite.CallAPI("PATCH", fmt.Sprintf("oauth/cf-eu10/v2/service_instances/%s?accepts_incomplete=true", iid),
+	response = suite.CallAPI(http.MethodPatch, fmt.Sprintf("oauth/cf-eu10/v2/service_instances/%s?accepts_incomplete=true", iid),
 		`{
 	   "service_id": "47c9dcbf-ff30-448e-ab36-d3bad66ba281",
 	   "plan_id": "7d55d31d-35ae-4438-bf13-6ffdfa107d9f",
@@ -242,8 +242,8 @@ func TestRemoveBindingsFromSuspended(t *testing.T) {
 	suite.WaitForOperationState(suspensionOpID, domain.Succeeded)
 
 	// when we remove bindings we just return OK
-	response = suite.CallAPI("DELETE", fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s?plan_id=361c511f-f939-4621-b228-d0fb79a1fe15&service_id=47c9dcbf-ff30-448e-ab36-d3bad66ba281", iid, bindingID1), "")
+	response = suite.CallAPI(http.MethodDelete, fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s?plan_id=361c511f-f939-4621-b228-d0fb79a1fe15&service_id=47c9dcbf-ff30-448e-ab36-d3bad66ba281", iid, bindingID1), "")
 	assert.Equal(t, http.StatusOK, response.StatusCode)
-	response = suite.CallAPI("DELETE", fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s?plan_id=361c511f-f939-4621-b228-d0fb79a1fe15&service_id=47c9dcbf-ff30-448e-ab36-d3bad66ba281", iid, bindingID2), "")
+	response = suite.CallAPI(http.MethodDelete, fmt.Sprintf("oauth/v2/service_instances/%s/service_bindings/%s?plan_id=361c511f-f939-4621-b228-d0fb79a1fe15&service_id=47c9dcbf-ff30-448e-ab36-d3bad66ba281", iid, bindingID2), "")
 	assert.Equal(t, http.StatusOK, response.StatusCode)
 }
