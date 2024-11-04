2cb41b47f2624b31d20f36156ee69d9159b55291
KsaweryZietara <91937141+KsaweryZietara@users.noreply.github.com>
2024-11-04 11:36:34 +0100

    wip (#1405)

--- cmd/broker/main.go
+++ cmd/broker/main.go
@@ -463,7 +463,6 @@ func createAPI(router *mux.Router, servicesConfig broker.ServicesConfig, planVal
 		broker.AttachRoutes(route, kymaEnvBroker, logger, cfg.Broker.Binding.CreateBindingTimeout)
 	}
 
-	//test
 	respWriter := httputil.NewResponseWriter(logs, cfg.DevelopmentMode)
 	runtimesInfoHandler := appinfo.NewRuntimeInfoHandler(db.Instances(), db.Operations(), defaultPlansConfig, cfg.DefaultRequestRegion, respWriter)
 	router.Handle("/info/runtimes", runtimesInfoHandler)
