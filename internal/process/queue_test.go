7f87ab623f4837416e602d4bbd5fc216cbde49a1
ralikio <74771103+ralikio@users.noreply.github.com>
2024-11-25 16:32:55 +0100

    Test Correction

--- internal/process/queue_test.go
+++ internal/process/queue_test.go
@@ -32,20 +32,20 @@ func TestWorkerLogging(t *testing.T) {
 		logger.SetOutput(&logs)
 
 		cancelContext, cancel := context.WithCancel(context.Background())
-		var waitGroup sync.WaitGroup
+		var waitForProcessing sync.WaitGroup
 
 		queue := NewQueue(&StdExecutor{logger: func(msg string) {
 			t.Log(msg)
-			waitGroup.Done()
+			waitForProcessing.Done()
 		}}, logger, "test", 10*time.Millisecond, 10*time.Millisecond)
-		queue.Run(cancelContext.Done(), 1)
 
-		waitGroup.Add(2)
+		waitForProcessing.Add(2)
 		queue.AddAfter("processId2", 0)
 		queue.Add("processId")
 		queue.SpeedUp(1)
+		queue.Run(cancelContext.Done(), 1)
 
-		waitGroup.Wait()
+		waitForProcessing.Wait()
 
 		queue.ShutDown()
 		cancel()
@@ -54,7 +54,7 @@ func TestWorkerLogging(t *testing.T) {
 		// then
 		stringLogs := logs.String()
 		t.Log(stringLogs)
-		require.True(t, strings.Contains(stringLogs, "msg=\"starting 1 worker(s), queue length is 0\" queueName=test"))
+		require.True(t, strings.Contains(stringLogs, "msg=\"starting 1 worker(s), queue length is 2\" queueName=test"))
 		require.True(t, strings.Contains(stringLogs, "msg=\"starting worker with id 0\" queueName=test workerId=0"))
 		require.True(t, strings.Contains(stringLogs, "msg=\"item processId2 will be added to the queue test after duration of 0, queue length is 1\" queueName=test"))
 		require.True(t, strings.Contains(stringLogs, "msg=\"added item processId to the queue test, queue length is 2\" queueName=test"))
@@ -85,11 +85,11 @@ func TestWorkerLogging(t *testing.T) {
 			t.Log(msg)
 			waitGroup.Done()
 		}}, logger, "test", 0, 0)
-		queue.Run(cancelContext.Done(), 1)
 
 		waitGroup.Add(2)
 		queue.Add("processId2")
 		queue.AddAfter("processId", 0)
+		queue.Run(cancelContext.Done(), 1)
 
 		waitGroup.Wait()
 
