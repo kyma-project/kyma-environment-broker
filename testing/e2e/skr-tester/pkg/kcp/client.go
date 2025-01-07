f1493f3c6e6440abf3cb3c0a142eabf5e93131a0
Marek Michali <marek.michali@sap.com>
2025-01-07 08:25:55 +0100

    Refactor

--- testing/e2e/skr-tester/pkg/kcp/client.go
+++ testing/e2e/skr-tester/pkg/kcp/client.go
@@ -32,7 +32,7 @@ func getEnvOrThrow(key string) string {
 }
 
 func NewKCPConfig() *KCPConfig {
-	config := &KCPConfig{
+	return &KCPConfig{
 		AuthType:          getEnvOrThrow("KCP_AUTH_TYPE"),
 		Host:              getEnvOrThrow("KCP_KEB_API_URL"),
 		IssuerURL:         getEnvOrThrow("KCP_OIDC_ISSUER_URL"),
@@ -42,17 +42,8 @@ func NewKCPConfig() *KCPConfig {
 		ClientID:          getEnvOrThrow("KCP_OIDC_CLIENT_ID"),
 		MotherShipApiUrl:  getEnvOrThrow("KCP_MOTHERSHIP_API_URL"),
 		KubeConfigApiUrl:  getEnvOrThrow("KCP_KUBECONFIG_API_URL"),
+		ClientSecret:      getEnvOrThrow("KCP_OIDC_CLIENT_SECRET"),
 	}
-
-	if clientSecret := os.Getenv("KCP_OIDC_CLIENT_SECRET"); clientSecret != "" {
-		config.ClientSecret = getEnvOrThrow("KCP_OIDC_CLIENT_SECRET")
-	} else {
-		config.OAuthClientID = getEnvOrThrow("KCP_OAUTH2_CLIENT_ID")
-		config.OAuthSecret = getEnvOrThrow("KCP_OAUTH2_CLIENT_SECRET")
-		config.OAuthIssuer = getEnvOrThrow("KCP_OAUTH2_ISSUER_URL")
-	}
-
-	return config
 }
 
 type KCPClient struct {
@@ -77,26 +68,18 @@ func (c *KCPClient) WriteConfigToFile() {
 
 	file.WriteString(fmt.Sprintf("auth-type: \"%s\"\n", c.Config.AuthType))
 	file.WriteString(fmt.Sprintf("gardener-namespace: \"%s\"\n", c.Config.GardenerNamespace))
-	if os.Getenv("KCP_OIDC_CLIENT_SECRET") != "" {
-		file.WriteString(fmt.Sprintf("oidc-client-id: \"%s\"\n", c.Config.ClientID))
-		file.WriteString(fmt.Sprintf("oidc-client-secret: %s\n", c.Config.ClientSecret))
-		file.WriteString(fmt.Sprintf("username: %s\n", c.Config.Username))
-	} else {
-		file.WriteString(fmt.Sprintf("oauth2-client-id: \"%s\"\n", c.Config.OAuthClientID))
-		file.WriteString(fmt.Sprintf("oauth2-client-secret: \"%s\"\n", c.Config.OAuthSecret))
-		file.WriteString(fmt.Sprintf("oauth2-issuer-url: \"%s\"\n", c.Config.OAuthIssuer))
-	}
-
+	file.WriteString(fmt.Sprintf("oidc-client-id: \"%s\"\n", c.Config.ClientID))
+	file.WriteString(fmt.Sprintf("oidc-client-secret: %s\n", c.Config.ClientSecret))
+	file.WriteString(fmt.Sprintf("username: %s\n", c.Config.Username))
 	file.WriteString(fmt.Sprintf("keb-api-url: \"%s\"\n", c.Config.Host))
 	file.WriteString(fmt.Sprintf("oidc-issuer-url: \"%s\"\n", c.Config.IssuerURL))
-	file.WriteString(fmt.Sprintf("mothership-api-url: \"%s\"\n", c.Config.MotherShipApiUrl))
 	file.WriteString(fmt.Sprintf("kubeconfig-api-url: \"%s\"\n", c.Config.KubeConfigApiUrl))
 }
 
 func (c *KCPClient) Login() error {
 	args := []string{"login", "--config", "config.yaml"}
 	if clientSecret := os.Getenv("KCP_OIDC_CLIENT_SECRET"); clientSecret != "" {
-		args = append(args, "-u", os.Getenv("KCP_TECH_USER_LOGIN"), "-p", os.Getenv("KCP_TECH_USER_PASSWORD"))
+		args = append(args, "-u", c.Config.Username, "-p", c.Config.Password)
 	}
 	_, err := exec.Command("kcp", args...).Output()
 	if err != nil {
