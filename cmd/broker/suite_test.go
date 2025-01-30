5af43d3c07f03df58eb4f74f365687463491f9b6
Marek Michali <marek.michali@sap.com>
2025-01-30 07:46:48 +0100

    Use std lib ServeMux

--- cmd/broker/suite_test.go
+++ cmd/broker/suite_test.go
@@ -40,7 +40,7 @@ import (
 	"github.com/kyma-project/kyma-environment-broker/internal/provisioner"
 	kebRuntime "github.com/kyma-project/kyma-environment-broker/internal/runtime"
 	"github.com/kyma-project/kyma-environment-broker/internal/storage"
-	"github.com/pivotal-cf/brokerapi/v8/domain"
+	"github.com/pivotal-cf/brokerapi/v12/domain"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
