86cfce71869d1a6a29f1845648d4d15edf3ffc6c
Marek Michali <marek.michali@sap.com>
2025-02-12 08:17:51 +0100

    Check size

--- internal/httputil/response.go
+++ internal/httputil/response.go
@@ -59,18 +59,20 @@ type ResponseRecorder struct {
 	http.ResponseWriter
 
 	StatusCode int
+	Size       int
 }
 
 func NewResponseRecorder(w http.ResponseWriter) *ResponseRecorder {
-
-	return &ResponseRecorder{ResponseWriter: w, StatusCode: http.StatusOK}
-
+	return &ResponseRecorder{ResponseWriter: w}
 }
 
 func (rr *ResponseRecorder) WriteHeader(code int) {
-
 	rr.StatusCode = code
-
 	rr.ResponseWriter.WriteHeader(code)
+}
 
+func (rr *ResponseRecorder) Write(b []byte) (int, error) {
+	size, err := rr.ResponseWriter.Write(b)
+	rr.Size += size
+	return size, err
 }
