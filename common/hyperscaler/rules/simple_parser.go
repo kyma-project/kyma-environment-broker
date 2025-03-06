1a22020e1a0f75db3e196c886e0d57f2cd51fe96
ralikio <74771103+ralikio@users.noreply.github.com>
2025-03-06 10:59:33 +0100

    Review Remarks
    https://github.com/kyma-project/kyma-environment-broker/pull/1811#discussion_r1981921650

--- common/hyperscaler/rules/simple_parser.go
+++ common/hyperscaler/rules/simple_parser.go
@@ -7,6 +7,14 @@ import (
 	"github.com/kyma-project/kyma-environment-broker/internal/broker"
 )
 
+const (
+	COMMA   = ","
+	ARROW   = "->"
+	L_PAREN = "("
+	R_PAREN = ")"
+	EQUAL   = "="
+)
+
 type SimpleParser struct {
 	enabledPlans *broker.EnablePlans
 }
@@ -16,7 +24,7 @@ func (g *SimpleParser) Parse(ruleEntry string) (*Rule, error) {
 
 	ruleEntry = RemoveWhitespaces(ruleEntry)
 
-	outputInputPart := strings.Split(ruleEntry, "->")
+	outputInputPart := strings.Split(ruleEntry, ARROW)
 
 	if len(outputInputPart) > 2 {
 		return nil, fmt.Errorf("rule has more than one arrows")
@@ -24,23 +32,23 @@ func (g *SimpleParser) Parse(ruleEntry string) (*Rule, error) {
 
 	inputPart := outputInputPart[0]
 
-	planAndInputAttr := strings.Split(inputPart, "(")
+	planAndInputAttr := strings.Split(inputPart, L_PAREN)
 
 	if len(planAndInputAttr) > 2 {
-		return nil, fmt.Errorf("rule has more than one '('")
+		return nil, fmt.Errorf("rule has more than one " + L_PAREN)
 	}
 
-	forValidationOnly := strings.Split(inputPart, ")")
+	forValidationOnly := strings.Split(inputPart, R_PAREN)
 
 	if len(forValidationOnly) > 2 {
-		return nil, fmt.Errorf("rule has more than one ')'")
+		return nil, fmt.Errorf("rule has more than one " + R_PAREN)
 	}
 
-	if strings.Contains(inputPart, "(") && !strings.Contains(inputPart, ")") {
+	if strings.Contains(inputPart, L_PAREN) && !strings.Contains(inputPart, R_PAREN) {
 		return nil, fmt.Errorf("rule has unclosed parentheses")
 	}
 
-	if !strings.Contains(inputPart, "(") && strings.Contains(inputPart, ")") {
+	if !strings.Contains(inputPart, L_PAREN) && strings.Contains(inputPart, R_PAREN) {
 		return nil, fmt.Errorf("rule has unclosed parentheses")
 	}
 
@@ -50,9 +58,9 @@ func (g *SimpleParser) Parse(ruleEntry string) (*Rule, error) {
 	}
 
 	if len(planAndInputAttr) > 1 {
-		inputPart := strings.TrimSuffix(planAndInputAttr[1], ")")
+		inputPart := strings.TrimSuffix(planAndInputAttr[1], R_PAREN)
 
-		inputAttrs := strings.Split(inputPart, ",")
+		inputAttrs := strings.Split(inputPart, COMMA)
 
 		for _, inputAttr := range inputAttrs {
 
@@ -60,7 +68,7 @@ func (g *SimpleParser) Parse(ruleEntry string) (*Rule, error) {
 				return nil, fmt.Errorf("input attribute is empty")
 			}
 
-			attribute := strings.Split(inputAttr, "=")
+			attribute := strings.Split(inputAttr, EQUAL)
 
 			if len(attribute) != 2 {
 				return nil, fmt.Errorf("input attribute has no value")
@@ -74,7 +82,7 @@ func (g *SimpleParser) Parse(ruleEntry string) (*Rule, error) {
 	}
 
 	if len(outputInputPart) > 1 {
-		outputAttrs := strings.Split(outputInputPart[1], ",")
+		outputAttrs := strings.Split(outputInputPart[1], COMMA)
 
 		for _, outputAttr := range outputAttrs {
 			if outputAttr == "" {
