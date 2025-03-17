ab7f79fb70648078a03898dbd5cc05096b8ac7e9
Marcin Szwed <marcin.szwed@sap.com>
2025-03-17 09:36:51 +0100

    Add method to create data for HAP parser

--- internal/process/provisioning/resolve_hyperscaler_account_credentials_secret.go
+++ internal/process/provisioning/resolve_hyperscaler_account_credentials_secret.go
@@ -2,12 +2,15 @@ package provisioning
 
 import (
 	"errors"
+	"fmt"
 	"log/slog"
 	"time"
 
 	"github.com/kyma-project/kyma-environment-broker/common/hyperscaler"
 	"github.com/kyma-project/kyma-environment-broker/common/hyperscaler/rules"
+	"github.com/kyma-project/kyma-environment-broker/common/runtime"
 	"github.com/kyma-project/kyma-environment-broker/internal"
+	"github.com/kyma-project/kyma-environment-broker/internal/broker"
 	kebError "github.com/kyma-project/kyma-environment-broker/internal/error"
 	"github.com/kyma-project/kyma-environment-broker/internal/process"
 	"github.com/kyma-project/kyma-environment-broker/internal/storage"
@@ -35,5 +38,33 @@ func (s *ResolveHyperscalerAccountCredentialsSecretStep) Name() string {
 }
 
 func (s *ResolveHyperscalerAccountCredentialsSecretStep) Run(operation internal.Operation, log *slog.Logger) (internal.Operation, time.Duration, error) {
+	attr, err := s.provisioningAttributesFromOperationData(operation)
+	if err != nil {
+		msg := fmt.Sprintf("%s for %s plan", err, broker.PlanNamesMapping[operation.ProvisioningParameters.PlanID])
+		return s.operationManager.OperationFailed(operation, msg, err, log)
+	}
+
+	s.rulesService.Match(attr)
+
 	return operation, 0, errors.New("not implemented")
+}
+
+func (s *ResolveHyperscalerAccountCredentialsSecretStep) provisioningAttributesFromOperationData(operation internal.Operation) (*rules.ProvisioningAttributes, error) {
+	cloudProvider := runtime.CloudProviderFromString(operation.ProviderValues.ProviderType)
+	effectiveRegion := getEffectiveRegionForSapConvergedCloud(operation.ProvisioningParameters.Parameters.Region)
+	hypType, err := hyperscaler.HypTypeFromCloudProviderWithRegion(cloudProvider, &effectiveRegion, &operation.ProvisioningParameters.PlatformRegion)
+	if err != nil {
+		return nil, err
+	}
+
+	return &rules.ProvisioningAttributes{
+		Plan:              broker.PlanNamesMapping[operation.ProvisioningParameters.PlanID],
+		PlatformRegion:    operation.ProvisioningParameters.PlatformRegion,
+		HyperscalerRegion: hypType.GetRegion(),
+		Hyperscaler:       hypType.GetKey(),
+	}, nil
+}
+
+func (s *ResolveHyperscalerAccountCredentialsSecretStep) buildLabelSelector(labels map[string]string) string {
+	return ""
 }
