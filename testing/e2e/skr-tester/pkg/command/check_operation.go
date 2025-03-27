76ad8ad91ae6744f7925d0172ee63cc531d674e6
KsaweryZietara <ksawery.zietara@sap.com>
2025-03-27 08:40:33 +0100

    Increase operation timeout in SKR-tester

--- testing/e2e/skr-tester/pkg/command/check_operation.go
+++ testing/e2e/skr-tester/pkg/command/check_operation.go
@@ -35,7 +35,7 @@ func NewCheckOperationCommand() *cobra.Command {
 
 	cobraCmd.Flags().StringVarP(&cmd.instanceID, "instanceID", "i", "", "Instance ID of the specific instance.")
 	cobraCmd.Flags().StringVarP(&cmd.operationID, "operationID", "o", "", "OperationID of the specific operation.")
-	cobraCmd.Flags().DurationVarP(&cmd.timeout, "timeout", "t", 40*time.Minute, "Timeout for the operation to finish.")
+	cobraCmd.Flags().DurationVarP(&cmd.timeout, "timeout", "t", 25*time.Hour, "Timeout for the operation to finish.")
 	cobraCmd.Flags().DurationVarP(&cmd.interval, "interval", "n", 1*time.Minute, "Interval between operation checks.")
 
 	return cobraCmd
