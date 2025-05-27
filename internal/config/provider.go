30e7e3d0a0a4b0399f928e714c1d17180766d6df
Marcin Szwed <marcin.szwed@sap.com>
2025-05-27 15:12:56 +0200

    Add decorator for config Provider to provide configs from ConfigMaps

--- internal/config/provider.go
+++ internal/config/provider.go
@@ -16,29 +16,51 @@ type (
 	Converter interface {
 		Convert(from string, to any) error
 	}
+
+	ConfigMapConfigProvider interface {
+		Provide(cfgKeyName string, cfgDestObj any) error
+	}
 )
 
+type configMapConfigProvider struct {
+	Provider
+	configMapName string
+	reqCfgKeys    string
+}
+
+func NewConfigMapConfigProvider(provider Provider, configMapName string, reqCfgKeys string) ConfigMapConfigProvider {
+	return &configMapConfigProvider{
+		Provider:      provider,
+		configMapName: configMapName,
+		reqCfgKeys:    reqCfgKeys,
+	}
+}
+
+func (p *configMapConfigProvider) Provide(cfgKeyName string, cfgDestObj any) error {
+	return p.Provider.Provide(p.configMapName, cfgKeyName, p.reqCfgKeys, cfgDestObj)
+}
+
 type provider struct {
-	Reader    Reader
-	Validator Validator
-	Converter Converter
+	reader    Reader
+	validator Validator
+	converter Converter
 }
 
 func NewConfigProvider(reader Reader, validator Validator, converter Converter) Provider {
-	return &provider{Reader: reader, Validator: validator, Converter: converter}
+	return &provider{reader: reader, validator: validator, converter: converter}
 }
 
 func (p *provider) Provide(cfgSrcName, cfgKeyName, reqCfgKeys string, cfgDestObj any) error {
-	cfgString, err := p.Reader.Read(cfgSrcName, cfgKeyName)
+	cfgString, err := p.reader.Read(cfgSrcName, cfgKeyName)
 	if err != nil {
 		return err
 	}
 
-	if err = p.Validator.Validate(reqCfgKeys, cfgString); err != nil {
+	if err = p.validator.Validate(reqCfgKeys, cfgString); err != nil {
 		return err
 	}
 
-	err = p.Converter.Convert(cfgString, cfgDestObj)
+	err = p.converter.Convert(cfgString, cfgDestObj)
 	if err != nil {
 		return err
 	}
