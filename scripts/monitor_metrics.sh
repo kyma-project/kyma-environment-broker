29f141492c9c4cc751c44b8c050600fe64fe2613
KsaweryZietara <ksawery.zietara@sap.com>
2025-05-30 14:29:23 +0200

    wip

--- scripts/monitor_metrics.sh
+++ scripts/monitor_metrics.sh
@@ -35,14 +35,14 @@ while true; do
   TIMESTAMP=$(date +%s)
   METRICS=$(curl -s http://localhost:30080/metrics)
 
-  GO_GOROUTINES=$(echo "$METRICS" | grep '^go_goroutines' | awk '{print $2}')
-  OPEN_FDS=$(echo "$METRICS" | grep '^process_open_fds' | awk '{print $2}')
-  DB_IDLE=$(echo "$METRICS" | grep 'go_sql_stats_connections_idle{db_name="broker"}' | awk '{print $2}')
-  DB_MAX_OPEN=$(echo "$METRICS" | grep 'go_sql_stats_connections_max_open{db_name="broker"}' | awk '{print $2}')
-  DB_IN_USE=$(echo "$METRICS" | grep 'go_sql_stats_connections_in_use{db_name="broker"}' | awk '{print $2}')
-  MEM_ALLOC=$(echo "$METRICS" | grep -w '^go_memstats_alloc_bytes' | awk '{printf "%.2f", $2/1048576}')
-  MEM_STACK=$(echo "$METRICS" | grep '^go_memstats_stack_inuse_bytes' | awk '{printf "%.2f", $2/1048576}')
-  MEM_HEAP=$(echo "$METRICS" | grep '^go_memstats_heap_inuse_bytes' | awk '{printf "%.2f", $2/1048576}')
+  GO_GOROUTINES=$(echo "$METRICS" | awk '/^go_goroutines/ {print $2}')
+  OPEN_FDS=$(echo "$METRICS" | awk '/^process_open_fds/ {print $2}')
+  DB_IDLE=$(echo "$METRICS" | awk '/go_sql_stats_connections_idle\{db_name="broker"\}/ {print $2}')
+  DB_MAX_OPEN=$(echo "$METRICS" | awk '/go_sql_stats_connections_max_open\{db_name="broker"\}/ {print $2}')
+  DB_IN_USE=$(echo "$METRICS" | awk '/go_sql_stats_connections_in_use\{db_name="broker"\}/ {print $2}')
+  MEM_ALLOC=$(echo "$METRICS" | awk '/^go_memstats_alloc_bytes / {printf "%.2f", $2/1048576}')
+  MEM_STACK=$(echo "$METRICS" | awk '/^go_memstats_stack_inuse_bytes/ {printf "%.2f", $2/1048576}')
+  MEM_HEAP=$(echo "$METRICS" | awk '/^go_memstats_heap_inuse_bytes/ {printf "%.2f", $2/1048576}')
 
   echo "{\"timestamp\": $TIMESTAMP, \"goroutines\": $GO_GOROUTINES, \"open_fds\": $OPEN_FDS, \"db_idle\": $DB_IDLE, \"db_max_open\": $DB_MAX_OPEN, \"db_in_use\": $DB_IN_USE, \"mem_alloc\": $MEM_ALLOC, \"mem_stack\": $MEM_STACK, \"mem_heap\": $MEM_HEAP}" >> "$METRICS_FILE"
 
