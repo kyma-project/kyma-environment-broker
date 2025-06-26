81e4fbb9efdd4ca3a00516945b61fbf6a99c2e5e
Marek Michali <marek.michali@sap.com>
2025-06-26 13:02:42 +0200

    Fix

--- internal/broker/broker.go
+++ internal/broker/broker.go
@@ -70,11 +70,8 @@ type Config struct {
 
 	RejectUnsupportedParameters bool `envconfig:"default=false"`
 	EnablePlanUpgrades          bool `envconfig:"default=false"`
-<<<<<<< support-jwks
 	EnableJwks                  bool `envconfig:"default=false"`
-=======
 	CheckQuotaLimit             bool `envconfig:"default=false"`
->>>>>>> main
 }
 
 type ServicesConfig map[string]Service
