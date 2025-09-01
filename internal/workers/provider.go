8a5a7a696a81f54a1d3ecef38543fa6260f6c00f
Marek Michali <marek.michali@sap.com>
2025-09-01 10:49:43 +0200

    Test setting labels, annotations and taints in RuntimeCR

--- internal/workers/provider.go
+++ internal/workers/provider.go
@@ -11,6 +11,7 @@ import (
 	"github.com/kyma-project/kyma-environment-broker/internal"
 	"github.com/kyma-project/kyma-environment-broker/internal/broker"
 	"github.com/kyma-project/kyma-environment-broker/internal/ptr"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
@@ -89,6 +90,32 @@ func (p *Provider) CreateAdditionalWorkers(values internal.ProviderValues, curre
 				Type:       ptr.String(values.DiskType),
 				VolumeSize: fmt.Sprintf("%sGi", volumeSize),
 			}
+		}
+
+		// Add hardcoded labels, annotations, and taints to the worker pool
+		worker.Labels = map[string]string{
+			"kyma.project.io/worker-pool": additionalWorkerNodePool.Name,
+			"environment":                 "production",
+			"team":                        "kyma",
+		}
+
+		worker.Annotations = map[string]string{
+			"kyma.project.io/created-by": "kyma-environment-broker",
+			"cluster.x-k8s.io/machine":   "managed",
+			"description":                "Additional worker node pool managed by KEB",
+		}
+
+		worker.Taints = []corev1.Taint{
+			{
+				Key:    "workload-type",
+				Value:  "additional-worker",
+				Effect: corev1.TaintEffectNoSchedule,
+			},
+			{
+				Key:    "managed-by",
+				Value:  "keb",
+				Effect: corev1.TaintEffectPreferNoSchedule,
+			},
 		}
 
 		workers = append(workers, worker)
