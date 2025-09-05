b334f393063e2e582c7b12f7a05475c249db3a8d
KsaweryZietara <ksawery.zietara@sap.com>
2025-09-05 11:41:54 +0200

    Use SubscriptionSecretName from instance

--- internal/process/provisioning/discover_available_zones_test.go
+++ internal/process/provisioning/discover_available_zones_test.go
@@ -20,6 +20,7 @@ func TestDiscoverAvailableZonesStep_ZonesDiscoveryDisabled(t *testing.T) {
 	memoryStorage := storage.NewMemoryStorage()
 
 	instance := fixInstance()
+	instance.SubscriptionSecretName = "secret-1"
 	err := memoryStorage.Instances().Insert(instance)
 	assert.NoError(t, err)
 
@@ -37,14 +38,11 @@ func TestDiscoverAvailableZonesStep_ZonesDiscoveryDisabled(t *testing.T) {
 			AutoScalerMax: 1,
 		},
 	}
-	targetSecret := "secret-1"
-	operation.ProvisioningParameters.Parameters.TargetSecret = &targetSecret
-
 	err = memoryStorage.Operations().InsertOperation(operation)
 	assert.NoError(t, err)
 
 	step := NewDiscoverAvailableZonesStep(
-		memoryStorage.Operations(),
+		memoryStorage,
 		newProviderSpec(t, false),
 		createGardenerClient(),
 		fixture.NewFakeAWSClientFactory(map[string][]string{
@@ -62,43 +60,12 @@ func TestDiscoverAvailableZonesStep_ZonesDiscoveryDisabled(t *testing.T) {
 	assert.Zero(t, repeat)
 }
 
-func TestDiscoverAvailableZonesStep_FailWhenNoTargetSecret(t *testing.T) {
-	// given
-	memoryStorage := storage.NewMemoryStorage()
-
-	instance := fixInstance()
-	err := memoryStorage.Instances().Insert(instance)
-	assert.NoError(t, err)
-
-	operation := fixture.FixProvisioningOperation(operationID, instanceID)
-	operation.RuntimeID = instance.RuntimeID
-	operation.ProvisioningParameters.PlatformProvider = pkg.AWS
-	operation.ProvisioningParameters.Parameters.AdditionalWorkerNodePools = []pkg.AdditionalWorkerNodePool{{
-		Name:          "worker-1",
-		MachineType:   "g6.xlarge",
-		HAZones:       false,
-		AutoScalerMin: 1,
-		AutoScalerMax: 1,
-	}}
-
-	err = memoryStorage.Operations().InsertOperation(operation)
-	assert.NoError(t, err)
-
-	step := NewDiscoverAvailableZonesStep(memoryStorage.Operations(), newProviderSpec(t, true), createGardenerClient(), fixture.NewFakeAWSClientFactory(map[string][]string{}, nil))
-
-	// when
-	operation, repeat, err := step.Run(operation, fixLogger())
-
-	// then
-	assert.EqualError(t, err, "target secret is missing")
-	assert.Zero(t, repeat)
-}
-
 func TestDiscoverAvailableZonesStep_FailWhenNoRegion(t *testing.T) {
 	// given
 	memoryStorage := storage.NewMemoryStorage()
 
 	instance := fixInstance()
+	instance.SubscriptionSecretName = "secret-1"
 	err := memoryStorage.Instances().Insert(instance)
 	assert.NoError(t, err)
 
@@ -112,14 +79,11 @@ func TestDiscoverAvailableZonesStep_FailWhenNoRegion(t *testing.T) {
 		AutoScalerMin: 1,
 		AutoScalerMax: 1,
 	}}
-	targetSecret := "secret-1"
-	operation.ProvisioningParameters.Parameters.TargetSecret = &targetSecret
 	operation.ProvisioningParameters.Parameters.Region = nil
-
 	err = memoryStorage.Operations().InsertOperation(operation)
 	assert.NoError(t, err)
 
-	step := NewDiscoverAvailableZonesStep(memoryStorage.Operations(), newProviderSpec(t, true), createGardenerClient(), fixture.NewFakeAWSClientFactory(map[string][]string{}, nil))
+	step := NewDiscoverAvailableZonesStep(memoryStorage, newProviderSpec(t, true), createGardenerClient(), fixture.NewFakeAWSClientFactory(map[string][]string{}, nil))
 
 	// when
 	operation, repeat, err := step.Run(operation, fixLogger())
@@ -129,7 +93,7 @@ func TestDiscoverAvailableZonesStep_FailWhenNoRegion(t *testing.T) {
 	assert.Zero(t, repeat)
 }
 
-func TestDiscoverAvailableZonesStep_RepeatWhenAWSError(t *testing.T) {
+func TestDiscoverAvailableZonesStep_FailWhenNoSubscriptionSecretName(t *testing.T) {
 	// given
 	memoryStorage := storage.NewMemoryStorage()
 
@@ -147,13 +111,42 @@ func TestDiscoverAvailableZonesStep_RepeatWhenAWSError(t *testing.T) {
 		AutoScalerMin: 1,
 		AutoScalerMax: 1,
 	}}
-	targetSecret := "secret-1"
-	operation.ProvisioningParameters.Parameters.TargetSecret = &targetSecret
-
 	err = memoryStorage.Operations().InsertOperation(operation)
 	assert.NoError(t, err)
 
-	step := NewDiscoverAvailableZonesStep(memoryStorage.Operations(), newProviderSpec(t, true), createGardenerClient(), fixture.NewFakeAWSClientFactory(map[string][]string{}, fmt.Errorf("AWS error")))
+	step := NewDiscoverAvailableZonesStep(memoryStorage, newProviderSpec(t, true), createGardenerClient(), fixture.NewFakeAWSClientFactory(map[string][]string{}, nil))
+
+	// when
+	operation, repeat, err := step.Run(operation, fixLogger())
+
+	// then
+	assert.EqualError(t, err, "subscription secret name is missing")
+	assert.Zero(t, repeat)
+}
+
+func TestDiscoverAvailableZonesStep_RepeatWhenAWSError(t *testing.T) {
+	// given
+	memoryStorage := storage.NewMemoryStorage()
+
+	instance := fixInstance()
+	instance.SubscriptionSecretName = "secret-1"
+	err := memoryStorage.Instances().Insert(instance)
+	assert.NoError(t, err)
+
+	operation := fixture.FixProvisioningOperation(operationID, instanceID)
+	operation.RuntimeID = instance.RuntimeID
+	operation.ProvisioningParameters.PlatformProvider = pkg.AWS
+	operation.ProvisioningParameters.Parameters.AdditionalWorkerNodePools = []pkg.AdditionalWorkerNodePool{{
+		Name:          "worker-1",
+		MachineType:   "g6.xlarge",
+		HAZones:       false,
+		AutoScalerMin: 1,
+		AutoScalerMax: 1,
+	}}
+	err = memoryStorage.Operations().InsertOperation(operation)
+	assert.NoError(t, err)
+
+	step := NewDiscoverAvailableZonesStep(memoryStorage, newProviderSpec(t, true), createGardenerClient(), fixture.NewFakeAWSClientFactory(map[string][]string{}, fmt.Errorf("AWS error")))
 
 	// when
 	operation, repeat, err := step.Run(operation, fixLogger())
@@ -163,7 +156,63 @@ func TestDiscoverAvailableZonesStep_RepeatWhenAWSError(t *testing.T) {
 	assert.Equal(t, 10*time.Second, repeat)
 }
 
-func TestDiscoverAvailableZonesStep_ProvisioningHappyPath(t *testing.T) {
+func TestDiscoverAvailableZonesStep_ProvisioningHappyPath_SubscriptionSecretNameFromInstance(t *testing.T) {
+	// given
+	memoryStorage := storage.NewMemoryStorage()
+
+	instance := fixInstance()
+	instance.SubscriptionSecretName = "secret-1"
+	err := memoryStorage.Instances().Insert(instance)
+	assert.NoError(t, err)
+
+	operation := fixture.FixProvisioningOperation(operationID, instanceID)
+	operation.RuntimeID = instance.RuntimeID
+	operation.ProvisioningParameters.PlatformProvider = pkg.AWS
+	machineType := "m6i.large"
+	operation.ProvisioningParameters.Parameters.MachineType = &machineType
+	operation.ProvisioningParameters.Parameters.AdditionalWorkerNodePools = []pkg.AdditionalWorkerNodePool{
+		{
+			Name:          "worker-1",
+			MachineType:   "g6.xlarge",
+			HAZones:       false,
+			AutoScalerMin: 1,
+			AutoScalerMax: 1,
+		},
+		{
+			Name:          "worker-2",
+			MachineType:   "g4dn.xlarge",
+			HAZones:       false,
+			AutoScalerMin: 1,
+			AutoScalerMax: 1,
+		},
+	}
+	err = memoryStorage.Operations().InsertOperation(operation)
+	assert.NoError(t, err)
+
+	step := NewDiscoverAvailableZonesStep(
+		memoryStorage,
+		newProviderSpec(t, true),
+		createGardenerClient(),
+		fixture.NewFakeAWSClientFactory(map[string][]string{
+			"m6i.large":   {"ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"},
+			"g6.xlarge":   {"ap-southeast-2a", "ap-southeast-2c"},
+			"g4dn.xlarge": {"ap-southeast-2b"},
+		}, nil),
+	)
+
+	// when
+	operation, repeat, err := step.Run(operation, fixLogger())
+
+	// then
+	assert.NoError(t, err)
+	assert.Zero(t, repeat)
+	assert.Len(t, operation.DiscoveredZones, 3)
+	assert.ElementsMatch(t, operation.DiscoveredZones["m6i.large"], []string{"ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"})
+	assert.ElementsMatch(t, operation.DiscoveredZones["g6.xlarge"], []string{"ap-southeast-2a", "ap-southeast-2c"})
+	assert.ElementsMatch(t, operation.DiscoveredZones["g4dn.xlarge"], []string{"ap-southeast-2b"})
+}
+
+func TestDiscoverAvailableZonesStep_ProvisioningHappyPath_SubscriptionSecretNameFromOperation(t *testing.T) {
 	// given
 	memoryStorage := storage.NewMemoryStorage()
 
@@ -192,14 +241,13 @@ func TestDiscoverAvailableZonesStep_ProvisioningHappyPath(t *testing.T) {
 			AutoScalerMax: 1,
 		},
 	}
-	targetSecret := "secret-1"
-	operation.ProvisioningParameters.Parameters.TargetSecret = &targetSecret
-
+	subscriptionSecretName := "secret-1"
+	operation.ProvisioningParameters.Parameters.TargetSecret = &subscriptionSecretName
 	err = memoryStorage.Operations().InsertOperation(operation)
 	assert.NoError(t, err)
 
 	step := NewDiscoverAvailableZonesStep(
-		memoryStorage.Operations(),
+		memoryStorage,
 		newProviderSpec(t, true),
 		createGardenerClient(),
 		fixture.NewFakeAWSClientFactory(map[string][]string{
@@ -226,6 +274,7 @@ func TestDiscoverAvailableZonesStep_UpdateHappyPath(t *testing.T) {
 	memoryStorage := storage.NewMemoryStorage()
 
 	instance := fixInstance()
+	instance.SubscriptionSecretName = "secret-1"
 	err := memoryStorage.Instances().Insert(instance)
 	assert.NoError(t, err)
 
@@ -248,14 +297,11 @@ func TestDiscoverAvailableZonesStep_UpdateHappyPath(t *testing.T) {
 			AutoScalerMax: 1,
 		},
 	}
-	targetSecret := "secret-1"
-	operation.ProvisioningParameters.Parameters.TargetSecret = &targetSecret
-
 	err = memoryStorage.Operations().InsertOperation(operation)
 	assert.NoError(t, err)
 
 	step := NewDiscoverAvailableZonesStep(
-		memoryStorage.Operations(),
+		memoryStorage,
 		newProviderSpec(t, true),
 		createGardenerClient(),
 		fixture.NewFakeAWSClientFactory(map[string][]string{
