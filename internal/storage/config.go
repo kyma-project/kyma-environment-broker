8ef4ea73eb91b2bbcf6f2a599343b07f937ec81f
Pieszka <jaroslaw.pieszka@sap.com>
2025-12-10 13:37:04 +0100

    batch size resized

--- internal/storage/config.go
+++ internal/storage/config.go
@@ -30,7 +30,7 @@ type Config struct {
 type FipsConfig struct {
 	WriteGcm         bool          `envconfig:"default=false"`
 	RewriteCfb       bool          `envconfig:"default=false"`
-	RewriteBatchSize int           `envconfig:"default=100"`
+	RewriteBatchSize int           `envconfig:"default=200"`
 	BatchInterval    time.Duration `envconfig:"default=1m"`
 }
 
