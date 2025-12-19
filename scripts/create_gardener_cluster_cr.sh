cac2b3249e86a0baf7411dce92966b96a79f91eb
Marek Michali <marek.michali@sap.com>
2025-12-19 15:40:49 +0100

    Fix name

--- scripts/create_gardener_cluster_cr.sh
+++ scripts/create_gardener_cluster_cr.sh
@@ -7,19 +7,20 @@ set -E          # needs to be set if we want the ERR trap
 set -o pipefail # prevents errors in a pipeline from being masked
 
 GLOBAL_ACCOUNT_ID=$1
+RUNTIME_ID=$2
 
-if [ -z "$GLOBAL_ACCOUNT_ID" ]; then
-  echo "Usage: $0 <global-account-id>"
+if [ -z "$GLOBAL_ACCOUNT_ID" ] || [ -z "$RUNTIME_ID" ]; then
+  echo "Usage: $0 <global-account-id> <runtime-id>"
   exit 1
 fi
 
-echo "Creating GardenerCluster with global account ID '$GLOBAL_ACCOUNT_ID' in namespace kcp-system..."
+echo "Creating GardenerCluster '$RUNTIME_ID' with global account ID '$GLOBAL_ACCOUNT_ID' in namespace kcp-system..."
 
 cat <<EOF | kubectl apply -f -
 apiVersion: infrastructuremanager.kyma-project.io/v1
 kind: GardenerCluster
 metadata:
-  name: test-gardener-cluster
+  name: $RUNTIME_ID
   namespace: kcp-system
   labels:
     kyma-project.io/global-account-id: $GLOBAL_ACCOUNT_ID
