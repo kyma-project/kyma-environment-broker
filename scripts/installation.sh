33646811f98f4760c75b327dcd794b7d0f338d7f
KsaweryZietara <ksawery.zietara@sap.com>
2026-01-14 15:25:54 +0100

    Use local images in E2E tests on PRs

--- scripts/installation.sh
+++ scripts/installation.sh
@@ -63,15 +63,17 @@ kubectl apply -f resources/installation/templates/
 # Deploy KEB helm chart
 cd resources/keb
 if [[ "$VERSION" == PR* ]]; then
+  # For PR versions, use local k3s registry
   helm install keb ../keb \
     --namespace kcp-system \
     -f ../../scripts/values.yaml \
     --set global.database.embedded.enabled=false \
     --set testConfig.kebDeployment.useAnnotations=true \
-    --set global.images.container_registry.path="europe-docker.pkg.dev/kyma-project/dev" \
+    --set global.images.container_registry.path="localhost:5000" \
     --set global.secrets.mechanism=secrets \
     --debug --wait
 else
+  # For release versions, use the production registry (from values.yaml default)
   helm install keb ../keb \
     --namespace kcp-system \
     -f ../../scripts/values.yaml \
