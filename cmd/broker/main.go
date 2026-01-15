d855dfba8d3b2b17754e2009ca895b2eec5ccbee
Miskiewicz <piotr.miskiewicz@sap.com>
2026-01-15 12:31:52 +0100

    wip

--- cmd/broker/main.go
+++ cmd/broker/main.go
@@ -263,7 +263,7 @@ func main() {
 
 	log.Info("Starting Kyma Environment Broker")
 
-	log.Info(fmt.Sprintf("Restrict to allowed GA IDS: %s", cfg.Broker.RestrictToAllowedGlobalAccounts))
+	log.Info(fmt.Sprintf("Restrict to allowed GA IDS: %v", cfg.Broker.RestrictToAllowedGlobalAccounts))
 
 	log.Info("Registering healthz endpoint for health probes")
 	health.NewServer(cfg.Broker.Host, cfg.Broker.StatusPort, log).ServeAsync()
