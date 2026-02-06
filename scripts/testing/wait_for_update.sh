3822a59d795d156cf0f7bef548c839586cf83f09
Marek Michali <marek.michali@sap.com>
2026-02-06 12:21:39 +0100

    Refactor

--- scripts/testing/wait_for_update.sh
+++ scripts/testing/wait_for_update.sh
@@ -1,7 +1,7 @@
 
 #!/usr/bin/env bash
 # Wait for updating to finish
-# Usage: wait_for_update.sh <instance_id>
+# Usage: wait_for_update.sh <instance_id> [base_url]
 
 # standard bash error handling
 set -o nounset  # treat unset variables as an error and exit immediately.
@@ -10,9 +10,11 @@ set -E          # needs to be set if we want the ERR trap
 set -o pipefail # prevents errors in a pipeline from being masked
 
 INSTANCE_ID=${1:?Instance ID required}
+BASE_URL=${2:-http://localhost:8080}
+
 while true; do
   STATUS=$(curl --request GET \
-    --url http://localhost:8080/runtimes \
+    --url "${BASE_URL}/runtimes" \
     --header 'Content-Type: application/json' \
     --header 'X-Broker-API-Version: 2.16' | jq -r --arg iid "$INSTANCE_ID" '.data[] | select(.instanceID==$iid) | .status.update.data[0].state')
 
