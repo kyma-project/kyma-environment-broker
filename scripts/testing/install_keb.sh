3101afaf9d987b91e084c28a1c0d58c5e1a2a4a3
Marek Michali <marek.michali@sap.com>
2026-02-08 11:00:17 +0100

    Install

--- scripts/testing/install_keb.sh
+++ scripts/testing/install_keb.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Install KEB chart (handles both release and PR versions)
-# Usage: install_keb.sh <is_release> <version>
+# Usage: install_keb.sh <is_release> [version]
 
 # standard bash error handling
 set -o nounset  # treat unset variables as an error and exit immediately.
@@ -9,12 +9,20 @@ set -E          # needs to be set if we want the ERR trap
 set -o pipefail # prevents errors in a pipeline from being masked
 
 IS_RELEASE=${1:?Is release flag required (true/false)}
-VERSION=${2:?Version required}
+VERSION=${2:-}
 
 if [ "$IS_RELEASE" == "true" ]; then
-  make install VERSION="$VERSION"
+  if [ -z "$VERSION" ]; then
+    make install
+  else
+    make install VERSION="$VERSION"
+  fi
 else
-  make install VERSION="PR-$VERSION" LOCAL_REGISTRY=true
+  if [ -z "$VERSION" ]; then
+    make install LOCAL_REGISTRY=true
+  else
+    make install VERSION="PR-$VERSION" LOCAL_REGISTRY=true
+  fi
 fi
 
-echo "KEB chart installed with version: $VERSION"
+echo "KEB chart installed with version: ${VERSION:-default}"
