e76e29f6a28621019251d51508bc6d6133a274d3
Jarosław Pieszka <jaroslaw.pieszka@sap.com>
2026-02-19 12:13:57 +0100

    Complexity for Update Runtime Step reduced  (#2893)
    * first extract
    
    * second extract
    
    * third extract
    
    * fourth extract - currentAdditionalWorkers
    
    * fifth getOIDCConfigs
    
    * local function used - index 19

--- internal/process/update/update_runtime_step.go
+++ internal/process/update/update_runtime_step.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	imv1 "github.com/kyma-project/infrastructure-manager/api/v1"
+	pkg "github.com/kyma-project/kyma-environment-broker/common/runtime"
 	"github.com/kyma-project/kyma-environment-broker/internal"
 	"github.com/kyma-project/kyma-environment-broker/internal/broker"
 	kebError "github.com/kyma-project/kyma-environment-broker/internal/error"
@@ -80,12 +81,7 @@ func (s *UpdateRuntimeStep) Run(operation internal.Operation, log *slog.Logger) 
 			return s.operationManager.OperationFailed(operation, fmt.Sprintf("while calculating plan specific values: %s", err), err, log)
 		}
 
-		currentAdditionalWorkers := make(map[string]gardener.Worker)
-		if runtime.Spec.Shoot.Provider.AdditionalWorkers != nil {
-			for _, worker := range *runtime.Spec.Shoot.Provider.AdditionalWorkers {
-				currentAdditionalWorkers[worker.Name] = worker
-			}
-		}
+		currentAdditionalWorkers := s.getCurrentAdditionalWorkers(runtime)
 
 		additionalWorkers, err := s.workersProvider.CreateAdditionalWorkers(values, currentAdditionalWorkers, operation.UpdatingParameters.AdditionalWorkerNodePools,
 			runtime.Spec.Shoot.Provider.Workers[0].Zones, operation.ProvisioningParameters.PlanID, operation.DiscoveredZones, log)
@@ -97,96 +93,42 @@ func (s *UpdateRuntimeStep) Run(operation internal.Operation, log *slog.Logger) 
 
 	if oidc := operation.UpdatingParameters.OIDC; oidc != nil {
 		if oidc.List != nil {
-			oidcConfigs := make([]imv1.OIDCConfig, 0)
-			for _, oidcConfig := range oidc.List {
-				requiredClaims := make(map[string]string)
-				for _, claim := range oidcConfig.RequiredClaims {
-					parts := strings.SplitN(claim, "=", 2)
-					if len(parts) == 2 {
-						requiredClaims[parts[0]] = parts[1]
-					}
-				}
-				oidcConfigObj := imv1.OIDCConfig{
-					OIDCConfig: gardener.OIDCConfig{
-						ClientID:       &oidcConfig.ClientID,
-						IssuerURL:      &oidcConfig.IssuerURL,
-						SigningAlgs:    oidcConfig.SigningAlgs,
-						GroupsClaim:    &oidcConfig.GroupsClaim,
-						UsernamePrefix: &oidcConfig.UsernamePrefix,
-						UsernameClaim:  &oidcConfig.UsernameClaim,
-						RequiredClaims: requiredClaims,
-						GroupsPrefix:   &oidcConfig.GroupsPrefix,
-					},
-				}
-				oidcConfigObj.JWKS, _ = base64.StdEncoding.DecodeString(oidcConfig.EncodedJwksArray)
-				oidcConfigs = append(oidcConfigs, oidcConfigObj)
-
-			}
-			runtime.Spec.Shoot.Kubernetes.KubeAPIServer.AdditionalOidcConfig = &oidcConfigs
+			runtime.Spec.Shoot.Kubernetes.KubeAPIServer.AdditionalOidcConfig = s.getOIDCConfigs(oidc)
 		} else if dto := oidc.OIDCConfigDTO; dto != nil {
 			if runtime.Spec.Shoot.Kubernetes.KubeAPIServer.AdditionalOidcConfig == nil {
 				runtime.Spec.Shoot.Kubernetes.KubeAPIServer.AdditionalOidcConfig = &[]imv1.OIDCConfig{{}}
 			}
 			config := &(*runtime.Spec.Shoot.Kubernetes.KubeAPIServer.AdditionalOidcConfig)[0]
-			if len(dto.SigningAlgs) > 0 {
-				config.SigningAlgs = dto.SigningAlgs
-			}
-			if dto.ClientID != "" {
-				config.ClientID = &dto.ClientID
-			}
-			if dto.IssuerURL != "" {
-				config.IssuerURL = &dto.IssuerURL
-			}
-			if dto.GroupsClaim != "" {
-				config.GroupsClaim = &dto.GroupsClaim
-			}
-			if dto.UsernamePrefix != "" {
-				config.UsernamePrefix = &dto.UsernamePrefix
-			}
-			if dto.UsernameClaim != "" {
-				config.UsernameClaim = &dto.UsernameClaim
-			}
-			if dto.GroupsPrefix != "" {
-				config.GroupsPrefix = &dto.GroupsPrefix
-			}
-			if len(dto.RequiredClaims) > 0 {
-				if len(dto.RequiredClaims) == 1 && dto.RequiredClaims[0] == "-" {
-					config.RequiredClaims = map[string]string{}
-				} else {
-					requiredClaims := make(map[string]string)
-					for _, claim := range dto.RequiredClaims {
-						parts := strings.SplitN(claim, "=", 2)
-						if len(parts) == 2 {
-							requiredClaims[parts[0]] = parts[1]
-						}
-					}
-					config.RequiredClaims = requiredClaims
+			assignIfNotEmpty := func(target **string, value string) {
+				if value != "" {
+					*target = &value
 				}
 			}
-			if dto.EncodedJwksArray == "-" {
+
+			config.SigningAlgs = dto.SigningAlgs
+			assignIfNotEmpty(&config.ClientID, dto.ClientID)
+			assignIfNotEmpty(&config.IssuerURL, dto.IssuerURL)
+			assignIfNotEmpty(&config.GroupsClaim, dto.GroupsClaim)
+			assignIfNotEmpty(&config.UsernamePrefix, dto.UsernamePrefix)
+			assignIfNotEmpty(&config.UsernameClaim, dto.UsernameClaim)
+			assignIfNotEmpty(&config.GroupsPrefix, dto.GroupsPrefix)
+
+			if len(dto.RequiredClaims) > 0 {
+				config.RequiredClaims = s.getRequiredClaims(dto)
+			}
+
+			switch dto.EncodedJwksArray {
+			case "-":
 				config.JWKS = nil
-			} else if dto.EncodedJwksArray != "" {
+			case "":
+				// Do nothing
+			default:
 				config.JWKS, _ = base64.StdEncoding.DecodeString(dto.EncodedJwksArray)
 			}
 		}
 	}
 
-	// operation.ProvisioningParameters were calculated and joined across provisioning and all update operations
-	if len(operation.ProvisioningParameters.Parameters.RuntimeAdministrators) != 0 {
-		// prepare new admins list for existing runtime
-		newAdministrators := make([]string, 0, len(operation.ProvisioningParameters.Parameters.RuntimeAdministrators))
-		newAdministrators = append(newAdministrators, operation.ProvisioningParameters.Parameters.RuntimeAdministrators...)
-
-		runtime.Spec.Security.Administrators = newAdministrators
-	} else {
-		if operation.ProvisioningParameters.ErsContext.UserID != "" {
-			// get default admin (user_id from provisioning operation)
-			runtime.Spec.Security.Administrators = []string{operation.ProvisioningParameters.ErsContext.UserID}
-		} else {
-			// some old clusters does not have a user_id
-			runtime.Spec.Security.Administrators = []string{}
-		}
-	}
+	runtime.Spec.Security.Administrators = s.getAdministrators(operation)
 
 	external := broker.IsExternalLicenseType(operation.ProvisioningParameters.ErsContext)
 	runtime.Spec.Security.Networking.Filter.Egress.Enabled = !external
@@ -209,4 +151,78 @@ func (s *UpdateRuntimeStep) Run(operation internal.Operation, log *slog.Logger) 
 	time.Sleep(s.delay)
 
 	return operation, 0, nil
+}
+
+func (s *UpdateRuntimeStep) getOIDCConfigs(oidc *pkg.OIDCConnectDTO) *[]imv1.OIDCConfig {
+	oidcConfigs := make([]imv1.OIDCConfig, 0)
+	for _, oidcConfig := range oidc.List {
+		oidcConfigs = append(oidcConfigs, s.getOIDCConfigObject(oidcConfig))
+	}
+	return &oidcConfigs
+}
+
+func (s *UpdateRuntimeStep) getCurrentAdditionalWorkers(runtime imv1.Runtime) map[string]gardener.Worker {
+	currentAdditionalWorkers := make(map[string]gardener.Worker)
+	if runtime.Spec.Shoot.Provider.AdditionalWorkers != nil {
+		for _, worker := range *runtime.Spec.Shoot.Provider.AdditionalWorkers {
+			currentAdditionalWorkers[worker.Name] = worker
+		}
+	}
+	return currentAdditionalWorkers
+}
+
+func (s *UpdateRuntimeStep) getRequiredClaims(dto *pkg.OIDCConfigDTO) map[string]string {
+	requiredClaims := make(map[string]string)
+	if !(len(dto.RequiredClaims) == 1 && dto.RequiredClaims[0] == "-") {
+		for _, claim := range dto.RequiredClaims {
+			parts := strings.SplitN(claim, "=", 2)
+			if len(parts) == 2 {
+				requiredClaims[parts[0]] = parts[1]
+			}
+		}
+	}
+	return requiredClaims
+}
+
+func (s *UpdateRuntimeStep) getOIDCConfigObject(oidcConfig pkg.OIDCConfigDTO) imv1.OIDCConfig {
+	requiredClaims := make(map[string]string)
+	for _, claim := range oidcConfig.RequiredClaims {
+		parts := strings.SplitN(claim, "=", 2)
+		if len(parts) == 2 {
+			requiredClaims[parts[0]] = parts[1]
+		}
+	}
+	oidcConfigObj := imv1.OIDCConfig{
+		OIDCConfig: gardener.OIDCConfig{
+			ClientID:       &oidcConfig.ClientID,
+			IssuerURL:      &oidcConfig.IssuerURL,
+			SigningAlgs:    oidcConfig.SigningAlgs,
+			GroupsClaim:    &oidcConfig.GroupsClaim,
+			UsernamePrefix: &oidcConfig.UsernamePrefix,
+			UsernameClaim:  &oidcConfig.UsernameClaim,
+			RequiredClaims: requiredClaims,
+			GroupsPrefix:   &oidcConfig.GroupsPrefix,
+		},
+	}
+	oidcConfigObj.JWKS, _ = base64.StdEncoding.DecodeString(oidcConfig.EncodedJwksArray)
+	return oidcConfigObj
+}
+
+func (s *UpdateRuntimeStep) getAdministrators(operation internal.Operation) []string {
+	// operation.ProvisioningParameters were calculated and joined across provisioning and all update operations
+	if len(operation.ProvisioningParameters.Parameters.RuntimeAdministrators) != 0 {
+		// prepare new admins list for existing runtime
+		newAdministrators := make([]string, 0, len(operation.ProvisioningParameters.Parameters.RuntimeAdministrators))
+		newAdministrators = append(newAdministrators, operation.ProvisioningParameters.Parameters.RuntimeAdministrators...)
+
+		return newAdministrators
+	} else {
+		if operation.ProvisioningParameters.ErsContext.UserID != "" {
+			// get default admin (user_id from provisioning operation)
+			return []string{operation.ProvisioningParameters.ErsContext.UserID}
+		} else {
+			// some old clusters does not have a user_id
+			return []string{}
+		}
+	}
 }
