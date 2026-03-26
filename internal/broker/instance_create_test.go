42c9736f75e2c51933d8e5edd5c0676deaa69cc4
Marcin Szwed <marcin.szwed@sap.com>
2026-03-26 20:00:13 +0100

    Replace more occurrences of ProvisionEndpoint constructor with builder

--- internal/broker/instance_create_test.go
+++ internal/broker/instance_create_test.go
@@ -3374,32 +3374,28 @@ func TestBtpRegionsMigrationSapConvergedCloud_NewRegion(t *testing.T) {
 	kcBuilder := &kcMock.KcBuilder{}
 	kcBuilder.On("GetServerURL", "").Return("", fmt.Errorf("error"))
 
-	provisionEndpoint := broker.NewProvision(
-		broker.Config{
+	provisionEndpoint := broker.NewFakeProvisionEndpointBuilder().
+		WithConfig(broker.Config{
 			EnablePlans:          []string{"sap-converged-cloud"},
 			URL:                  brokerURL,
-			OnlySingleTrialPerGA: true,
-		},
-		gardener.Config{Project: "test", ShootDomain: "example.com", DNSProviders: fixDNSProviders()},
-		imConfigFixture,
-		memoryStorage,
-		queue,
-		broker.PlansConfig{},
-		log,
-		dashboardConfig,
-		kcBuilder,
-		whitelist.Set{},
-		newSchemaService(t),
-		newProviderSpec(t),
-		fixValueProvider(t),
-		config.FakeProviderConfigProvider{},
-		nil,
-		nil,
-		nil,
-		nil,
-		nil,
-		map[string]string{"cf-eu10": "cf-eu01"},
-	)
+			OnlySingleTrialPerGA: true}).
+		WithGardenerConfig(gardener.Config{
+			Project:      "test",
+			ShootDomain:  "example.com",
+			DNSProviders: fixDNSProviders()}).
+		WithInfrastructureManager(imConfigFixture).
+		WithStorage(memoryStorage).
+		WithQueue(queue).
+		WithLogger(log).
+		WithDashboardConfig(dashboardConfig).
+		WithKubeconfigBuilder(kcBuilder).
+		WithFreemiumWhitelist(whitelist.Set{}).
+		WithSchemaService(newSchemaService(t)).
+		WithConfigurationProvider(newProviderSpec(t)).
+		WithValuesProvider(fixValueProvider(t)).
+		WithConfigMapConfigProvider(config.FakeProviderConfigProvider{}).
+		WithBtpRegionsMigrationSapConvergedCloud(map[string]string{"cf-eu10": "cf-eu01"}).
+		Build()
 
 	// when
 	_, err := provisionEndpoint.Provision(
