1f6d4b93e9ffb2b8a31d6930386f788a502ce051
Marcin Szwed <marcin.szwed@sap.com>
2026-03-26 14:47:35 +0100

    Add unit test - update instance on gvisor update

--- internal/broker/instance_update_input_params_test.go
+++ internal/broker/instance_update_input_params_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"testing"
 
+	pkg "github.com/kyma-project/kyma-environment-broker/common/runtime"
+	"github.com/kyma-project/kyma-environment-broker/internal"
 	"github.com/pivotal-cf/brokerapi/v12/domain"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -52,4 +54,22 @@ func TestUpdatingParameters_Gvisor(t *testing.T) {
 		require.NotNil(t, params.Gvisor)
 		assert.False(t, params.Gvisor.Enabled)
 	})
+}
+
+func TestPersistenceOnUpdate_Gvisor(t *testing.T) {
+	endpoint := &UpdateEndpoint{}
+	gvisor := &pkg.GvisorDTO{Enabled: true}
+
+	// given
+	instance := &internal.Instance{}
+	params := &internal.UpdatingParametersDTO{Gvisor: gvisor}
+	operation := &internal.Operation{}
+	details := domain.UpdateDetails{}
+
+	// when
+	_, err := endpoint.updateInstanceAndOperationParameters(instance, params, operation, details, internal.ERSContext{}, fixLogger())
+
+	// then
+	require.NoError(t, err)
+	assert.Equal(t, gvisor, instance.Parameters.Parameters.Gvisor)
 }
