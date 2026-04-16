aa6d48df344d52b364d95e07a56bee6c9651d443
Jarosław Pieszka <jaroslaw.pieszka@sap.com>
2026-04-16 10:51:05 +0200

    defer added

--- common/hyperscaler/rules/rules_service.go
+++ common/hyperscaler/rules/rules_service.go
@@ -26,7 +26,7 @@ func NewRulesServiceFromFile(rulesFilePath string, allowedPlans sets.Set[string]
 	if err != nil {
 		return nil, fmt.Errorf("failed to open file: %s", err)
 	}
-	defer file.Close()
+	defer func() { _ = file.Close() }()
 	rulesConfig := &RulesConfig{}
 	if err := rulesConfig.LoadFromFile(file); err != nil {
 		return nil, err
