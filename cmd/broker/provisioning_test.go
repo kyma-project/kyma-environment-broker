49468d826e4523a0677ed1daf5eec93e2dab9e85
Miskiewicz <piotr.miskiewicz@sap.com>
2026-04-29 11:32:19 +0200

    wip

--- cmd/broker/provisioning_test.go
+++ cmd/broker/provisioning_test.go
@@ -3730,9 +3730,9 @@ func TestProvisioningWithMaxPods(t *testing.T) {
 		// then
 		suite.WaitForOperationState(opID, domain.Succeeded)
 		runtime := suite.GetRuntimeResourceByInstanceID(iid)
-		assert.Equal(t, int32(250), *runtime.Spec.Shoot.Provider.Workers[0].Kubernetes.Kubelet.MaxPods)
+		assert.Equal(t, int32(200), *runtime.Spec.Shoot.Provider.Workers[0].Kubernetes.Kubelet.MaxPods)
 		for _, additionalWorker := range *runtime.Spec.Shoot.Provider.AdditionalWorkers {
-			assert.Equal(t, int32(250), *additionalWorker.Kubernetes.Kubelet.MaxPods)
+			assert.Equal(t, int32(200), *additionalWorker.Kubernetes.Kubelet.MaxPods)
 		}
 	})
 
