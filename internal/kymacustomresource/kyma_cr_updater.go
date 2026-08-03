bc35d55afbe97f8f354705fbe0465d5d52944012
Jeremi Szczotka <j.szczotka@sap.com>
2026-08-03 12:51:01 +0200

    Use Generic patchCRs to Eliminate Code Duplication in Updater

--- internal/kymacustomresource/kyma_cr_updater.go
+++ internal/kymacustomresource/kyma_cr_updater.go
@@ -77,58 +77,43 @@ func (u *Updater) Run() error {
 }
 
 func (u *Updater) patchKymaCRs(subaccountID, betaEnabled, usedForProduction string, ctx context.Context) bool {
-	list, err := u.k8sClient.Resource(u.kymaGVR).Namespace(namespace).List(ctx, metav1.ListOptions{
-		LabelSelector: fmt.Sprintf(subaccountIdLabelFormat, subaccountID),
-	})
-	if err != nil {
-		u.logger.Warn("while listing Kyma CRs: " + err.Error() + " requeue item")
-		return true
-	}
-	if len(list.Items) == 0 {
-		u.logger.Info("no Kyma CRs found for subaccount " + subaccountID)
-		return false
-	}
-	u.logger.Debug(fmt.Sprintf("found %d Kyma CRs for subaccount", len(list.Items)))
-	retryRequired := false
-	for _, un := range list.Items {
-		labels := un.GetLabels()
-		if labels == nil {
-			labels = make(map[string]string)
-		}
-		labels[BetaEnabledLabelKey] = betaEnabled
-		labels[UsedForProductionLabelKey] = usedForProduction
-		un.SetLabels(labels)
-		if _, err := u.k8sClient.Resource(u.kymaGVR).Namespace(namespace).Update(ctx, &un, metav1.UpdateOptions{}); err != nil {
-			u.logger.Warn("while updating Kyma CR: " + err.Error() + " item will be added back to the queue")
-			retryRequired = true
-		}
-	}
-	return retryRequired
+	return u.patchCRs(u.kymaGVR, "Kyma", subaccountID, map[string]string{
+		BetaEnabledLabelKey:       betaEnabled,
+		UsedForProductionLabelKey: usedForProduction,
+	}, ctx)
 }
 
 func (u *Updater) patchRuntimeCRs(subaccountID, usedForProduction string, ctx context.Context) bool {
-	list, err := u.k8sClient.Resource(u.runtimeGVR).Namespace(namespace).List(ctx, metav1.ListOptions{
+	return u.patchCRs(u.runtimeGVR, "Runtime", subaccountID, map[string]string{
+		UsedForProductionLabelKey: usedForProduction,
+	}, ctx)
+}
+
+func (u *Updater) patchCRs(gvr schema.GroupVersionResource, resourceName, subaccountID string, labelsToSet map[string]string, ctx context.Context) bool {
+	list, err := u.k8sClient.Resource(gvr).Namespace(namespace).List(ctx, metav1.ListOptions{
 		LabelSelector: fmt.Sprintf(subaccountIdLabelFormat, subaccountID),
 	})
 	if err != nil {
-		u.logger.Warn("while listing Runtime CRs: " + err.Error() + " requeue item")
+		u.logger.Warn("while listing " + resourceName + " CRs: " + err.Error() + " requeue item")
 		return true
 	}
 	if len(list.Items) == 0 {
-		u.logger.Info("no Runtime CRs found for subaccount " + subaccountID)
+		u.logger.Info("no " + resourceName + " CRs found for subaccount " + subaccountID)
 		return false
 	}
-	u.logger.Debug(fmt.Sprintf("found %d Runtime CRs for subaccount", len(list.Items)))
+	u.logger.Debug(fmt.Sprintf("found %d %s CRs for subaccount", len(list.Items), resourceName))
 	retryRequired := false
 	for _, un := range list.Items {
 		labels := un.GetLabels()
 		if labels == nil {
 			labels = make(map[string]string)
 		}
-		labels[UsedForProductionLabelKey] = usedForProduction
+		for k, v := range labelsToSet {
+			labels[k] = v
+		}
 		un.SetLabels(labels)
-		if _, err := u.k8sClient.Resource(u.runtimeGVR).Namespace(namespace).Update(ctx, &un, metav1.UpdateOptions{}); err != nil {
-			u.logger.Warn("while updating Runtime CR: " + err.Error() + " item will be added back to the queue")
+		if _, err := u.k8sClient.Resource(gvr).Namespace(namespace).Update(ctx, &un, metav1.UpdateOptions{}); err != nil {
+			u.logger.Warn("while updating " + resourceName + " CR: " + err.Error() + " item will be added back to the queue")
 			retryRequired = true
 		}
 	}
