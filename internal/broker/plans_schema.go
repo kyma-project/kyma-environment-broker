59fa1af0f3db714084c1b2a56654658a5e056d6f
Krupinski <piotr.krupinski@sap.com>
2026-08-17 12:54:59 +0200

    Wip

--- internal/broker/plans_schema.go
+++ internal/broker/plans_schema.go
@@ -909,12 +909,12 @@ func NewTaintsSchema(rejectUnsupportedParameters bool) *TaintsType {
 					Type:        "string",
 					MinLength:   1,
 					Description: "Specifies the taint key.",
-					Pattern:     `^([a-zA-Z0-9]([-a-zA-Z0-9_.]{0,251}[a-zA-Z0-9])?/)?([a-zA-Z0-9][-a-zA-Z0-9_.]*)?[a-zA-Z0-9]$`,
+					Pattern:     `^([a-zA-Z0-9]([-a-zA-Z0-9_.]{0,251}[a-zA-Z0-9])?/)?[a-zA-Z0-9]([-a-zA-Z0-9_.]{0,61}[a-zA-Z0-9])?$`,
 				},
 				Value: Type{
 					Type:        "string",
 					Description: "Specifies the taint value.",
-					Pattern:     `^([a-zA-Z0-9]([-a-zA-Z0-9_.]*[a-zA-Z0-9])?)?$`,
+					Pattern:     `^([a-zA-Z0-9]([-a-zA-Z0-9_.]{0,61}[a-zA-Z0-9])?)?$`,
 				},
 				Effect: Type{
 					Type:        "string",
@@ -943,10 +943,10 @@ func NewLabelsSchema() *KeyValueType {
 		PatternProperties: map[string]interface{}{
 			".*": map[string]interface{}{
 				"type":    "string",
-				"pattern": `^([a-zA-Z0-9]([-a-zA-Z0-9_.]*[a-zA-Z0-9])?)?$`,
+				"pattern": `^([a-zA-Z0-9]([-a-zA-Z0-9_.]{0,61}[a-zA-Z0-9])?)?$`,
 			},
 		},
-		PropertyNames: map[string]interface{}{"pattern": `^([a-zA-Z0-9]([-a-zA-Z0-9_.]{0,251}[a-zA-Z0-9])?/)?([a-zA-Z0-9][-a-zA-Z0-9_.]*)?[a-zA-Z0-9]$`},
+		PropertyNames: map[string]interface{}{"pattern": `^([a-zA-Z0-9]([-a-zA-Z0-9_.]{0,251}[a-zA-Z0-9])?/)?[a-zA-Z0-9]([-a-zA-Z0-9_.]{0,61}[a-zA-Z0-9])?$`},
 	}
 }
 
@@ -958,7 +958,7 @@ func NewAnnotationsSchema() *KeyValueType {
 		PatternProperties: map[string]interface{}{
 			".*": map[string]interface{}{"type": "string"},
 		},
-		PropertyNames: map[string]interface{}{"pattern": `^([a-zA-Z0-9]([-a-zA-Z0-9_.]{0,251}[a-zA-Z0-9])?/)?([a-zA-Z0-9][-a-zA-Z0-9_.]*)?[a-zA-Z0-9]$`},
+		PropertyNames: map[string]interface{}{"pattern": `^([a-zA-Z0-9]([-a-zA-Z0-9_.]{0,251}[a-zA-Z0-9])?/)?[a-zA-Z0-9]([-a-zA-Z0-9_.]{0,61}[a-zA-Z0-9])?$`},
 	}
 }
 
